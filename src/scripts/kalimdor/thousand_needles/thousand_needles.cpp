144c239703c089f86dd8d7970f5fd068711f3059
thumsoul <thumsoul@localhost>
2010-10-10 21:59:09 +0200

    [Oregon] Scripts for quests 4904 and 4966

--- src/scripts/kalimdor/thousand_needles/thousand_needles.cpp
+++ src/scripts/kalimdor/thousand_needles/thousand_needles.cpp
@@ -17,7 +17,7 @@
 /* ScriptData
 SDName: Thousand Needles
 SD%Complete: 100
-SDComment: Support for Quest: 4770, 1950, 5151.
+SDComment: Support for Quest: 1950, 4770, 4904, 4966, 5151.
 SDCategory: Thousand Needles
 EndScriptData */
 
@@ -26,6 +26,8 @@ npc_swiftmountain
 npc_plucky
 go_panther_cage
 npc_enraged_panther
+npc_kanati
+npc_lakota_windsong
 EndContentData */
 
 #include "precompiled.h"
@@ -35,12 +37,12 @@ EndContentData */
 ## npc_swiftmountain
 ######*/
 
-#define SAY_START -1000147
-#define SAY_WYVERN -1000148
-#define SAY_COMPLETE -1000149
+#define SAY_START 				-1000147
+#define SAY_WYVERN 				-1000148
+#define SAY_COMPLETE 			-1000149
 
-#define QUEST_HOMEWARD_BOUND 4770
-#define ENTRY_WYVERN 4107
+#define QUEST_HOMEWARD_BOUND 	4770
+#define ENTRY_WYVERN 			4107
 
 struct npc_swiftmountainAI : public npc_escortAI
 {
@@ -61,10 +63,10 @@ npc_swiftmountainAI(Creature *c) : npc_escortAI(c) {}
             m_creature->SummonCreature(ENTRY_WYVERN, -5001.98, -934.96, -5.55, 3.18,TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 25000);
             m_creature->SummonCreature(ENTRY_WYVERN, -4999.06, -949.61, -5.42, 2.04,TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 25000);
             break;
-         case 26:
+        case 26:
             DoScriptText(SAY_COMPLETE, m_creature);
             break;
-         case 27:
+        case 27:
             player->GroupEventHappens(QUEST_HOMEWARD_BOUND,m_creature);
             break;
         }
@@ -170,14 +172,14 @@ struct npc_pluckyAI : public ScriptedAI
             else Timer-=diff;
         }
 
-       if(Chicken)
-       {
-           if (ChickenTimer < diff)
-           {
-               m_creature->HandleEmoteCommand(EMOTE_ONESHOT_WAVE);
-               Chicken = false;
-           }else ChickenTimer-=diff;
-       }
+		if(Chicken)
+		{
+			if (ChickenTimer < diff)
+			{
+				m_creature->HandleEmoteCommand(EMOTE_ONESHOT_WAVE);
+				Chicken = false;
+			}else ChickenTimer-=diff;
+		}
 
         if(!UpdateVictim())
             return;
@@ -276,6 +278,158 @@ CreatureAI* GetAI_npc_enraged_panther(Creature* pCreature)
     return new npc_enraged_pantherAI(pCreature);
 }
 
+/*#####
+# npc_kanati
+######*/
+
+enum eKanati
+{
+    SAY_KAN_START              = -1600410,
+
+    QUEST_PROTECT_KANATI        = 4966,
+    NPC_GALAK_ASS               = 10720
+};
+
+const float m_afGalakLoc[] = { -4867.387695, -1357.353760, -48.226 };
+
+struct npc_kanatiAI : public npc_escortAI
+{
+    npc_kanatiAI(Creature* pCreature) : npc_escortAI(pCreature) { }
+
+    void Reset() {}
+    
+    void Aggro(Unit *pWho) {}
+
+    void WaypointReached(uint32 uiPointId)
+    {
+        switch(uiPointId)
+        {
+            case 0:
+                DoScriptText(SAY_KAN_START, me);
+                DoSpawnGalak();
+                break;
+            case 1:
+                if (Player* pPlayer = GetPlayerForEscort())
+                    pPlayer->GroupEventHappens(QUEST_PROTECT_KANATI, me);
+                break;
+        }
+    }
+
+    void DoSpawnGalak()
+    {
+        for (int i = 0; i < 3; ++i)
+            me->SummonCreature(NPC_GALAK_ASS,
+            m_afGalakLoc[0], m_afGalakLoc[1], m_afGalakLoc[2], 0.0f,
+            TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 25000);
+    }
+
+    void JustSummoned(Creature* pSummoned)
+    {
+        pSummoned->AI()->AttackStart(me);
+    }
+};
+
+CreatureAI* GetAI_npc_kanati(Creature* pCreature)
+{
+    return new npc_kanatiAI(pCreature);
+}
+
+bool QuestAccept_npc_kanati(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
+{
+    if (pQuest->GetQuestId() == QUEST_PROTECT_KANATI)
+        ((npc_escortAI*)(pCreature->AI()))->Start(false, false, false, pPlayer->GetGUID(), pCreature->GetEntry());
+
+    return true;
+}
+
+/*######
+# npc_lakota_windsong
+######*/
+
+enum eLakota
+{
+    SAY_LAKO_START              = -1600365,
+    SAY_LAKO_LOOK_OUT           = -1600366,
+    SAY_LAKO_HERE_COME          = -1600367,
+    SAY_LAKO_MORE               = -1600368,
+    SAY_LAKO_END                = -1600369,
+
+    QUEST_FREE_AT_LAST          = 4904,
+    NPC_GRIM_BANDIT             = 10758,
+    FACTION_ESCORTEE_LAKO       = 232,                      //guessed
+
+    ID_AMBUSH_1                 = 0,
+    ID_AMBUSH_2                 = 2,
+    ID_AMBUSH_3                 = 4
+};
+
+float m_afBanditLoc[6][6]=
+{
+    {-4905.479492, -2062.732666, 84.352},
+    {-4915.201172, -2073.528320, 84.733},
+    {-4878.883301, -1986.947876, 91.966},
+    {-4877.503906, -1966.113403, 91.859},
+    {-4767.985352, -1873.169189, 90.192},
+    {-4788.861328, -1888.007813, 89.888}
+};
+
+struct npc_lakota_windsongAI : public npc_escortAI
+{
+    npc_lakota_windsongAI(Creature* pCreature) : npc_escortAI(pCreature) {}
+
+    void Reset() {}
+    
+    void Aggro(Unit *pWho) {}
+
+    void WaypointReached(uint32 uiPointId)
+    {
+        switch(uiPointId)
+        {
+            case 8:
+                DoScriptText(SAY_LAKO_LOOK_OUT, me);
+                DoSpawnBandits(ID_AMBUSH_1);
+                break;
+            case 14:
+                DoScriptText(SAY_LAKO_HERE_COME, me);
+                DoSpawnBandits(ID_AMBUSH_2);
+                break;
+            case 21:
+                DoScriptText(SAY_LAKO_MORE, me);
+                DoSpawnBandits(ID_AMBUSH_3);
+                break;
+            case 45:
+                if (Player* pPlayer = GetPlayerForEscort())
+                    pPlayer->GroupEventHappens(QUEST_FREE_AT_LAST, me);
+                break;
+        }
+    }
+
+    void DoSpawnBandits(int uiAmbushId)
+    {
+        for (int i = 0; i < 2; ++i)
+            me->SummonCreature(NPC_GRIM_BANDIT,
+            m_afBanditLoc[i+uiAmbushId][0], m_afBanditLoc[i+uiAmbushId][1], m_afBanditLoc[i+uiAmbushId][2], 0.0f,
+            TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 60000);
+    }
+};
+
+CreatureAI* GetAI_npc_lakota_windsong(Creature* pCreature)
+{
+    return new npc_lakota_windsongAI(pCreature);
+}
+
+bool QuestAccept_npc_lakota_windsong(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
+{
+    if (pQuest->GetQuestId() == QUEST_FREE_AT_LAST)
+    {
+        DoScriptText(SAY_LAKO_START, pCreature, pPlayer);
+        pCreature->setFaction(FACTION_ESCORTEE_LAKO);
+
+        ((npc_escortAI*)(pCreature->AI()))->Start(false, false, false, pPlayer->GetGUID(), pCreature->GetEntry());
+    }
+    return true;
+}
+
 /*######
 ## AddSC
 ######*/
@@ -307,5 +461,16 @@ void AddSC_thousand_needles()
     newscript->Name = "npc_enraged_panther";
     newscript->GetAI = &GetAI_npc_enraged_panther;
     newscript->RegisterSelf();
-}
+    
+    newscript = new Script;
+    newscript->Name = "npc_kanati";
+    newscript->GetAI = &GetAI_npc_kanati;
+    newscript->pQuestAccept = &QuestAccept_npc_kanati;
+    newscript->RegisterSelf();
 
+	newscript = new Script;
+    newscript->Name = "npc_lakota_windsong";
+    newscript->GetAI = &GetAI_npc_lakota_windsong;
+    newscript->pQuestAccept = &QuestAccept_npc_lakota_windsong;
+    newscript->RegisterSelf();
+}
