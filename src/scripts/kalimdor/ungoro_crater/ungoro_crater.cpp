7ebfd4208bd0947322ca098151edb625a581e922
thumsoul <thumsoul@localhost>
2010-10-10 22:10:48 +0200

    [Oregon] Script for quest 4491

--- src/scripts/kalimdor/ungoro_crater/ungoro_crater.cpp
+++ src/scripts/kalimdor/ungoro_crater/ungoro_crater.cpp
@@ -17,16 +17,18 @@
 /* ScriptData
 SDName: Ungoro Crater
 SD%Complete: 100
-SDComment: Support for Quest: 4245
+SDComment: Support for Quest: 4245, 4491
 SDCategory: Ungoro Crater
 EndScriptData */
 
 /* ContentData
 npc_a-me
+npc_ringo
 EndContentData */
 
 #include "precompiled.h"
 #include "EscortAI.h"
+#include "FollowerAI.h"
 
 #define SAY_READY -1000200
 #define SAY_AGGRO1 -1000201
@@ -42,6 +44,9 @@ EndContentData */
 #define ENTRY_TARLORD1 6519
 #define ENTRY_STOMPER 6513
 
+/*######
+## npc_a-me
+######*/
 
 struct npc_ameAI : public npc_escortAI
 {
@@ -137,6 +142,212 @@ CreatureAI* GetAI_npc_ame(Creature *pCreature)
     return new npc_ameAI(pCreature);
 }
 
+/*######
+## npc_ringo
+######*/
+
+enum eRingo
+{
+    SAY_RIN_START_1             = -1000416,
+    SAY_RIN_START_2             = -1000417,
+
+    SAY_FAINT_1                 = -1000418,
+    SAY_FAINT_2                 = -1000419,
+    SAY_FAINT_3                 = -1000420,
+    SAY_FAINT_4                 = -1000421,
+
+    SAY_WAKE_1                  = -1000422,
+    SAY_WAKE_2                  = -1000423,
+    SAY_WAKE_3                  = -1000424,
+    SAY_WAKE_4                  = -1000425,
+
+    SAY_RIN_END_1               = -1000426,
+    SAY_SPR_END_2               = -1000427,
+    SAY_RIN_END_3               = -1000428,
+    EMOTE_RIN_END_4             = -1000429,
+    EMOTE_RIN_END_5             = -1000430,
+    SAY_RIN_END_6               = -1000431, // signed for 6784
+    SAY_SPR_END_7               = -1000432,
+    EMOTE_RIN_END_8             = -1000433,
+
+    SPELL_REVIVE_RINGO          = 15591,
+    QUEST_A_LITTLE_HELP         = 4491,
+    NPC_SPRAGGLE                = 9997,
+    FACTION_ESCORTEE            = 113
+};
+
+struct npc_ringoAI : public FollowerAI
+{
+    npc_ringoAI(Creature* pCreature) : FollowerAI(pCreature) { }
+
+    uint32 m_uiFaintTimer;
+    uint32 m_uiEndEventProgress;
+    uint32 m_uiEndEventTimer;
+
+    uint64 SpraggleGUID;
+
+    void Reset()
+    {
+        m_uiFaintTimer = urand(30000, 60000);
+        m_uiEndEventProgress = 0;
+        m_uiEndEventTimer = 1000;
+        SpraggleGUID = 0;
+    }
+    
+    void Aggro(Unit *pWho) {}
+
+    void MoveInLineOfSight(Unit *pWho)
+    {
+        FollowerAI::MoveInLineOfSight(pWho);
+
+        if (!me->getVictim() && !HasFollowState(STATE_FOLLOW_COMPLETE) && pWho->GetEntry() == NPC_SPRAGGLE)
+        {
+            if (me->IsWithinDistInMap(pWho, INTERACTION_DISTANCE))
+            {
+                if (Player* pPlayer = GetLeaderForFollower())
+                {
+                    if (pPlayer->GetQuestStatus(QUEST_A_LITTLE_HELP) == QUEST_STATUS_INCOMPLETE)
+                        pPlayer->GroupEventHappens(QUEST_A_LITTLE_HELP, me);
+                }
+
+                SpraggleGUID = pWho->GetGUID();
+                SetFollowComplete(true);
+            }
+        }
+    }
+
+    void SpellHit(Unit* pCaster, const SpellEntry* pSpell)
+    {
+        if (HasFollowState(STATE_FOLLOW_INPROGRESS | STATE_FOLLOW_PAUSED) && pSpell->Id == SPELL_REVIVE_RINGO)
+            ClearFaint();
+    }
+
+    void SetFaint()
+    {
+        if (!HasFollowState(STATE_FOLLOW_POSTEVENT))
+        {
+            SetFollowPaused(true);
+
+            DoScriptText(RAND(SAY_FAINT_1,SAY_FAINT_2,SAY_FAINT_3,SAY_FAINT_4), me);
+        }
+
+        //what does actually happen here? Emote? Aura?
+        me->SetStandState(UNIT_STAND_STATE_SLEEP);
+    }
+
+    void ClearFaint()
+    {
+        me->SetStandState(UNIT_STAND_STATE_STAND);
+
+        if (HasFollowState(STATE_FOLLOW_POSTEVENT))
+            return;
+
+        DoScriptText(RAND(SAY_WAKE_1,SAY_WAKE_2,SAY_WAKE_3,SAY_WAKE_4), me);
+
+        SetFollowPaused(false);
+    }
+
+    void UpdateFollowerAI(const uint32 uiDiff)
+    {
+        if (!UpdateVictim())
+        {
+            if (HasFollowState(STATE_FOLLOW_POSTEVENT))
+            {
+                if (m_uiEndEventTimer <= uiDiff)
+                {
+                    Unit *pSpraggle = Unit::GetUnit(*me, SpraggleGUID);
+                    if (!pSpraggle || !pSpraggle->isAlive())
+                    {
+                        SetFollowComplete();
+                        return;
+                    }
+
+                    switch(m_uiEndEventProgress)
+                    {
+                        case 1:
+                            DoScriptText(SAY_RIN_END_1, me);
+                            m_uiEndEventTimer = 3000;
+                            break;
+                        case 2:
+                            DoScriptText(SAY_SPR_END_2, pSpraggle);
+                            m_uiEndEventTimer = 5000;
+                            break;
+                        case 3:
+                            DoScriptText(SAY_RIN_END_3, me);
+                            m_uiEndEventTimer = 1000;
+                            break;
+                        case 4:
+                            DoScriptText(EMOTE_RIN_END_4, me);
+                            SetFaint();
+                            m_uiEndEventTimer = 9000;
+                            break;
+                        case 5:
+                            DoScriptText(EMOTE_RIN_END_5, me);
+                            ClearFaint();
+                            m_uiEndEventTimer = 1000;
+                            break;
+                        case 6:
+                            DoScriptText(SAY_RIN_END_6, me);
+                            m_uiEndEventTimer = 3000;
+                            break;
+                        case 7:
+                            DoScriptText(SAY_SPR_END_7, pSpraggle);
+                            m_uiEndEventTimer = 10000;
+                            break;
+                        case 8:
+                            DoScriptText(EMOTE_RIN_END_8, me);
+                            m_uiEndEventTimer = 5000;
+                            break;
+                        case 9:
+                            SetFollowComplete();
+                            break;
+                    }
+
+                    ++m_uiEndEventProgress;
+                }
+                else
+                    m_uiEndEventTimer -= uiDiff;
+            }
+            else if (HasFollowState(STATE_FOLLOW_INPROGRESS))
+            {
+                if (!HasFollowState(STATE_FOLLOW_PAUSED))
+                {
+                    if (m_uiFaintTimer <= uiDiff)
+                    {
+                        SetFaint();
+                        m_uiFaintTimer = urand(60000, 120000);
+                    }
+                    else
+                        m_uiFaintTimer -= uiDiff;
+                }
+            }
+
+            return;
+        }
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_npc_ringo(Creature* pCreature)
+{
+    return new npc_ringoAI(pCreature);
+}
+
+bool QuestAccept_npc_ringo(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
+{
+    if (pQuest->GetQuestId() == QUEST_A_LITTLE_HELP)
+    {
+        if (npc_ringoAI* pRingoAI = CAST_AI(npc_ringoAI, pCreature->AI()))
+        {
+            pCreature->SetStandState(UNIT_STAND_STATE_STAND);
+            pRingoAI->StartFollow(pPlayer, FACTION_ESCORTEE, pQuest);
+        }
+    }
+
+    return true;
+}
+
 void AddSC_ungoro_crater()
 {
     Script *newscript;
@@ -145,6 +356,12 @@ void AddSC_ungoro_crater()
     newscript->Name = "npc_ame";
     newscript->GetAI = &GetAI_npc_ame;
     newscript->pQuestAccept = &QuestAccept_npc_ame;
+    newscript->RegisterSelf();
+    
+    newscript = new Script;
+    newscript->Name = "npc_ringo";
+    newscript->GetAI = &GetAI_npc_ringo;
+    newscript->pQuestAccept = &QuestAccept_npc_ringo;
     newscript->RegisterSelf();
 }
 
