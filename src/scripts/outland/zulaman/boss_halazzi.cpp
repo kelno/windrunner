cbacbe7ad43bed410dcbe1e5986ac9ecdd08dfd0
thumsoul <thumsoul@localhost>
2010-12-02 15:43:46 +0100

    Previous commits: 80 yards sounds better than 50

--- src/scripts/outland/zulaman/boss_halazzi.cpp
+++ src/scripts/outland/zulaman/boss_halazzi.cpp
@@ -267,7 +267,7 @@ struct boss_halazziAI : public ScriptedAI
 
             if(ShockTimer < diff)
             {
-                if(Unit* target = SelectUnit(SELECT_TARGET_RANDOM,0,50.0f,true))
+                if(Unit* target = SelectUnit(SELECT_TARGET_RANDOM,0,80.0f,true))
                 {
                     if(target->IsNonMeleeSpellCasted(false))
                         DoCast(target,SPELL_EARTHSHOCK);
