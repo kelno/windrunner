282b53d131fcc0bd890d161454c642738d1597e6
thumsoul <thumsoul@localhost>
2010-12-02 15:58:18 +0100

    Hyjal/Rage Winterchill: Added copyright notice

--- src/scripts/kalimdor/caverns_of_time/hyjal/boss_rage_winterchill.cpp
+++ src/scripts/kalimdor/caverns_of_time/hyjal/boss_rage_winterchill.cpp
@@ -1,3 +1,24 @@
+/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+/* ScriptData
+SDName: boss_rage_winterchill
+SD%Complete: 100
+SDCategory: Hyjal
+EndScriptData */
 
 #include "precompiled.h"
 #include "def_hyjal.h"
@@ -176,7 +197,7 @@ struct boss_rage_winterchillAI : public hyjal_trashAI
         }else NovaTimer -= diff;
         if(IceboltTimer < diff)
         {
-            DoCast(SelectUnit(SELECT_TARGET_RANDOM,0,40,true), SPELL_ICEBOLT);
+            DoCast(SelectUnit(SELECT_TARGET_RANDOM,0,40.0f,true), SPELL_ICEBOLT);
             IceboltTimer = 11000+rand()%20000;
         }else IceboltTimer -= diff;
         if(BerserkTimer < diff && !Enraged)
