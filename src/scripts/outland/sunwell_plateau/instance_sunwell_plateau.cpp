57fcab36edd5493f7fd667727d3d030d2afb381c
thumsoul <thumsoul@localhost>
2010-12-18 23:34:38 +0100

    Sunwell Plateau/Gauntlet: Implemented path && Removed debug logs

--- src/scripts/outland/sunwell_plateau/instance_sunwell_plateau.cpp
+++ src/scripts/outland/sunwell_plateau/instance_sunwell_plateau.cpp
@@ -318,12 +318,18 @@ struct instance_sunwell_plateau : public ScriptedInstance
             float x, y, z;
             
             Commander->GetNearPoint(Commander, x, y, z, 1.0f, 1.0f, 0);
-            if (Creature *Bringer1 = Commander->SummonCreature(MOB_SOULBINDER, x, y, z, Commander->GetOrientation(), TEMPSUMMON_MANUAL_DESPAWN, 0))
-                Bringer1->MonsterSay("[DEBUG] First bringer!", LANG_UNIVERSAL, NULL);
+            if (Creature *Bringer1 = Commander->SummonCreature(MOB_SOULBINDER, x, y, z, Commander->GetOrientation(), TEMPSUMMON_MANUAL_DESPAWN, 0)) {
+                //Bringer1->MonsterSay("[DEBUG] First bringer!", LANG_UNIVERSAL, NULL);
+                Bringer1->setActive(true);
+                Bringer1->GetMotionMaster()->MovePath(25851, false);
+            }
             
             Commander->GetNearPoint(Commander, x, y, z, 1.0f, 1.0f, 0);
-            if (Creature *Bringer2 = Commander->SummonCreature(MOB_DEATHBRINGER, x, y, z, Commander->GetOrientation(), TEMPSUMMON_MANUAL_DESPAWN, 0))
-                Bringer2->MonsterSay("[DEBUG] Second bringer!", LANG_UNIVERSAL, NULL);
+            if (Creature *Bringer2 = Commander->SummonCreature(MOB_DEATHBRINGER, x, y, z, Commander->GetOrientation(), TEMPSUMMON_MANUAL_DESPAWN, 0)) {
+                //Bringer2->MonsterSay("[DEBUG] Second bringer!", LANG_UNIVERSAL, NULL);
+                Bringer2->setActive(true);
+                Bringer2->GetMotionMaster()->MovePath(25851, false);
+            }
                 
             BringersTimer = 40000;
         }
@@ -334,8 +340,11 @@ struct instance_sunwell_plateau : public ScriptedInstance
             float x, y, z;
             
             Commander->GetNearPoint(Commander, x, y, z, 1.0f, 1.0f, 0);
-            if (Creature *Fiend = Commander->SummonCreature(MOB_VOLATILE_FIEND, x, y, z, Commander->GetOrientation(), TEMPSUMMON_MANUAL_DESPAWN, 0))
-                Fiend->MonsterSay("[DEBUG] Fiend!", LANG_UNIVERSAL, NULL);
+            if (Creature *Fiend = Commander->SummonCreature(MOB_VOLATILE_FIEND, x, y, z, Commander->GetOrientation(), TEMPSUMMON_MANUAL_DESPAWN, 0)) {
+                //Fiend->MonsterSay("[DEBUG] Fiend!", LANG_UNIVERSAL, NULL);
+                Fiend->setActive(true);
+                Fiend->GetMotionMaster()->MovePath(25851, false);
+            }
                 
             FiendTimer = 12000;
         }
