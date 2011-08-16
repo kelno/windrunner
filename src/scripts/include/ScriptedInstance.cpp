1f2828b974cebc6b8adcf88cb5eca573d6731d85
thumsoul <thumsoul@localhost>
2011-08-16 14:39:22 +0200

    Fixed npc 23602 flag and melee damage

---
+++ src/scripts/include/ScriptedInstance.cpp
@@ -0,0 +1,32 @@
+#include "ScriptedInstance.h"
+#include "../../game/Player.h"
+#include "../../game/Map.h"
+#include "../../game/MapReference.h"
+
+void ScriptedInstance::CastOnAllPlayers(uint32 spellId)
+{
+    Map::PlayerList const& players = instance->GetPlayers();
+
+    if (!players.isEmpty())
+    {
+        for(Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
+        {
+            if (Player* plr = itr->getSource())
+                plr->CastSpell(plr, spellId, true);                
+        }
+    }
+}
+
+void ScriptedInstance::RemoveAuraOnAllPlayers(uint32 spellId)
+{
+    Map::PlayerList const& players = instance->GetPlayers();
+
+    if (!players.isEmpty())
+    {
+        for(Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
+        {
+            if (Player* plr = itr->getSource())
+                plr->RemoveAurasDueToSpell(spellId);
+        }
+    }
+}
