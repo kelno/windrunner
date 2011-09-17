51a8c7a386d4485a2fbece5108950a53668fe148
thumsoul <thumsoul@localhost>
2011-09-17 20:01:18 +0200

    Implemented EventMap stuff, more hooks in CreatureAINew, azuregos new script is now complete

--- src/scripts/outdoor/boss_azuregos.cpp
+++ src/scripts/outdoor/boss_azuregos.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+/* Copyright (C) 2009 - 2011 Windrunner
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -14,171 +14,128 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-/* ScriptData
-SDName: Boss_Azuregos
-SD%Complete: 90
-SDComment: Teleport not included, spell reflect not effecting dots (Core problem)
-SDCategory: Azshara
-EndScriptData */
-
 #include "precompiled.h"
 
-#define SAY_TELEPORT            -1000100
+enum {
+    SAY_TELEPORT            = -1000100,
 
-#define SPELL_MARKOFFROST        23182
-#define SPELL_MANASTORM          21097
-#define SPELL_CHILL              21098
-#define SPELL_FROSTBREATH        21099
-#define SPELL_REFLECT            22067
-#define SPELL_CLEAVE              8255                      //Perhaps not right ID
-#define SPELL_ENRAGE             23537
-
-struct boss_azuregosAI : public ScriptedAI
-{
-    boss_azuregosAI(Creature *c) : ScriptedAI(c) {}
-
-    uint32 MarkOfFrost_Timer;
-    uint32 ManaStorm_Timer;
-    uint32 Chill_Timer;
-    uint32 Breath_Timer;
-    uint32 Teleport_Timer;
-    uint32 Reflect_Timer;
-    uint32 Cleave_Timer;
-    uint32 Enrage_Timer;
-    bool Enraged;
-
-    void Reset()
-    {
-        MarkOfFrost_Timer = 35000;
-        ManaStorm_Timer = 5000 + rand()%12000;
-        Chill_Timer = 10000 + rand()%20000;
-        Breath_Timer = 2000 + rand()%6000;
-        Teleport_Timer = 30000;
-        Reflect_Timer = 15000 + rand()%15000;
-        Cleave_Timer = 7000;
-        Enrage_Timer = 0;
-        Enraged = false;
-    }
-
-    void Aggro(Unit *who) {}
-
-    void UpdateAI(const uint32 diff)
-    {
-        //Return since we have no target
-        if (!UpdateVictim() )
-            return;
-
-        if(Teleport_Timer < diff)
-        {
-            DoScriptText(SAY_TELEPORT, m_creature);
-            std::list<HostilReference*>& m_threatlist = m_creature->getThreatManager().getThreatList();
-            std::list<HostilReference*>::iterator i = m_threatlist.begin();
-            for (i = m_threatlist.begin(); i!= m_threatlist.end();++i)
-            {
-                Unit* pUnit = Unit::GetUnit((*m_creature), (*i)->getUnitGuid());
-                if(pUnit && (pUnit->GetTypeId() == TYPEID_PLAYER))
-                {
-                    DoTeleportPlayer(pUnit, m_creature->GetPositionX(), m_creature->GetPositionY(), m_creature->GetPositionZ()+3, pUnit->GetOrientation());
-                }
-            }
-
-            DoResetThreat();
-            Teleport_Timer = 30000;
-        }else Teleport_Timer -= diff;
-
-        //        //MarkOfFrost_Timer
-        //        if (MarkOfFrost_Timer < diff)
-        //        {
-        //            DoCast(m_creature->getVictim(),SPELL_MARKOFFROST);
-        //            MarkOfFrost_Timer = 25000;
-        //        }else MarkOfFrost_Timer -= diff;
-
-        //Chill_Timer
-        if (Chill_Timer < diff)
-        {
-            DoCast(m_creature->getVictim(),SPELL_CHILL);
-            Chill_Timer = 13000 + rand()%12000;
-        }else Chill_Timer -= diff;
-
-        //Breath_Timer
-        if (Breath_Timer < diff)
-        {
-            DoCast(m_creature->getVictim(),SPELL_FROSTBREATH);
-            Breath_Timer = 10000 + rand()%5000;
-        }else Breath_Timer -= diff;
-
-        //ManaStorm_Timer
-        if (ManaStorm_Timer < diff)
-        {
-            if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM,0))
-                DoCast(target,SPELL_MANASTORM);
-            ManaStorm_Timer = 7500 + rand()%5000;
-        }else ManaStorm_Timer -= diff;
-
-        //Reflect_Timer
-        if (Reflect_Timer < diff)
-        {
-            DoCast(m_creature,SPELL_REFLECT);
-            Reflect_Timer = 20000 + rand()%15000;
-        }else Reflect_Timer -= diff;
-
-        //Cleave_Timer
-        if (Cleave_Timer < diff)
-        {
-            DoCast(m_creature->getVictim(),SPELL_CLEAVE);
-            Cleave_Timer = 7000;
-        }else Cleave_Timer -= diff;
-
-        //Enrage_Timer
-        if (m_creature->GetHealth()*100 / m_creature->GetMaxHealth() < 26 && !Enraged)
-        {
-            DoCast(m_creature, SPELL_ENRAGE);
-            Enraged = true;
-        }
-
-        DoMeleeAttackIfReady();
-    }
+    SPELL_MARK_OF_FROST     = 23182,
+    SPELL_AURA_OF_FROST     = 23186,
+    SPELL_MANASTORM         = 21097,
+    SPELL_CHILL             = 21098,
+    SPELL_FROSTBREATH       = 21099,
+    SPELL_REFLECT           = 22067,
+    SPELL_CLEAVE            = 8255,                     // Perhaps not right ID
+    SPELL_ENRAGE            = 23537
 };
 
-CreatureAI* GetAI_boss_azuregos(Creature *_Creature)
-{
-    return new boss_azuregosAI (_Creature);
-}
+enum {
+    EV_MANASTORM,
+    EV_CHILL,
+    EV_BREATH,
+    EV_TELEPORT,
+    EV_REFLECT,
+    EV_CLEAVE
+};
 
 class Boss_azuregos : public CreatureScript
 {
     public:
         Boss_azuregos() : CreatureScript("boss_azuregos_new") {}
 
-        class boss_azuregos_newAI : public CreatureAINew
+        class Boss_azuregos_newAI : public CreatureAINew
         {
             public:
-                boss_azuregos_newAI(Creature* creature) : CreatureAINew(creature) {}
+                Boss_azuregos_newAI(Creature* creature) : CreatureAINew(creature) {}
                 
-                void onCombatStart(Unit* victim)
+                void onReset(bool onSpawn)
                 {
-                    sLog.outString("AGGRO");
+                    schedule(EV_MANASTORM, 5000, 17000);
+                    schedule(EV_CHILL, 10000, 30000);
+                    schedule(EV_BREATH, 2000, 8000);
+                    schedule(EV_TELEPORT, 30000);
+                    schedule(EV_REFLECT, 15000, 30000);
+                    schedule(EV_CLEAVE, 7000);
+                    
+                    enraged = false;
+                }
+                
+                void onKill(Unit* killed)
+                {
+                    me->AddAura(SPELL_MARK_OF_FROST, killed);
+                }
+                
+                void onThreatAdd(Unit* who, float& threat)
+                {
+                    if (who->HasAura(SPELL_MARK_OF_FROST) && !who->HasAura(SPELL_AURA_OF_FROST))
+                        who->CastSpell(me, SPELL_AURA_OF_FROST, true);
                 }
 
                 void update(uint32 const diff)
                 {
+                    if (!updateVictim())
+                        return;
+
+                    updateEvents(diff);
+                        
+                    if (me->IsBelowHPPercent(25.0f) && !enraged) {
+                        doCast(me, SPELL_ENRAGE, true);
+                        enraged = true;
+                    }
+
+                    while (executeEvent(diff, m_currEvent)) {
+                        switch (m_currEvent) {
+                        case EV_MANASTORM:
+                        {
+                            doCast(selectUnit(TARGET_RANDOM, 0), SPELL_MANASTORM);
+                            schedule(EV_MANASTORM, 7500, 12500);
+                            break;
+                        }
+                        case EV_CHILL:
+                        {
+                            doCast(me->getVictim(), SPELL_CHILL);
+                            schedule(EV_CHILL, 13000, 25000);
+                            break;
+                        }
+                        case EV_BREATH:
+                        {
+                            doCast(me->getVictim(), SPELL_FROSTBREATH);
+                            schedule(EV_BREATH, 1000, 15000);
+                            break;
+                        }
+                        case EV_TELEPORT:
+                        {
+                            break;
+                        }
+                        case EV_REFLECT:
+                        {
+                            doCast(me, SPELL_REFLECT);
+                            schedule(EV_REFLECT, 20000, 35000);
+                            break;
+                        }
+                        case EV_CLEAVE:
+                        {
+                            doCast(me->getVictim(), SPELL_CLEAVE);
+                            schedule(EV_CLEAVE, 7000);
+                            break;
+                        }
+                        }
+                    }
                     
+                    doMeleeAttackIfReady();
                 }
+                
+            private:
+                bool enraged;
         };
         
         CreatureAINew* getAI(Creature* creature)
         {
-            return new boss_azuregos_newAI(creature);
+            return new Boss_azuregos_newAI(creature);
         }
 };
 
 void AddSC_boss_azuregos()
 {
-    Script *newscript;
-    newscript = new Script;
-    newscript->Name="boss_azuregos";
-    newscript->GetAI = &GetAI_boss_azuregos;
-    newscript->RegisterSelf();
-    
-    sScriptMgr.addScript(new Boss_azuregos);
+    sScriptMgr.addScript(new Boss_azuregos());
 }
