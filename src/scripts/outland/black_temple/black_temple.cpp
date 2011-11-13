54dfc6b097bdc4c3b37c7ca8a14512f11a1f3bc3
thumsoul <thumsoul@localhost>
2011-11-13 17:00:13 +0100

    Black Temple: Added (and translated) teleportation to Spirit of Olum

--- src/scripts/outland/black_temple/black_temple.cpp
+++ src/scripts/outland/black_temple/black_temple.cpp
@@ -34,7 +34,9 @@ EndContentData */
 ####*/
 
 #define SPELL_TELEPORT      41566                           // s41566 - Teleport to Ashtongue NPC's
-#define GOSSIP_OLUM1        "Teleport me to the other Ashtongue Deathsworn"
+#define GOSSIP_OLUM1        "Téléportez-moi au Sanctuaire des ombres."
+#define SPELL_TELEPORT_UDALO    41570
+#define GOSSIP_UDALO            "Teleportez-moi au Conseil Illidari."
 
 bool GossipHello_npc_spirit_of_olum(Player* player, Creature* _Creature)
 {
@@ -42,6 +44,8 @@ bool GossipHello_npc_spirit_of_olum(Player* player, Creature* _Creature)
 
     if(pInstance && (pInstance->GetData(DATA_SUPREMUSEVENT) >= DONE) && (pInstance->GetData(DATA_HIGHWARLORDNAJENTUSEVENT) >= DONE))
         player->ADD_GOSSIP_ITEM(0, GOSSIP_OLUM1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+    if(pInstance && (pInstance->GetData(DATA_ILLIDARICOUNCILEVENT) >= DONE))
+        player->ADD_GOSSIP_ITEM(0, GOSSIP_UDALO, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
 
     player->SEND_GOSSIP_MENU(_Creature->GetNpcTextId(), _Creature->GetGUID());
     return true;
@@ -49,20 +53,22 @@ bool GossipHello_npc_spirit_of_olum(Player* player, Creature* _Creature)
 
 bool GossipSelect_npc_spirit_of_olum(Player* player, Creature* _Creature, uint32 sender, uint32 action)
 {
-    if(action == GOSSIP_ACTION_INFO_DEF + 1)
-        player->CLOSE_GOSSIP_MENU();
+    player->CLOSE_GOSSIP_MENU();
+    if(action == GOSSIP_ACTION_INFO_DEF + 1) {
+        player->InterruptNonMeleeSpells(false);
+        player->CastSpell(player, SPELL_TELEPORT, false);
+    }
+    else if (action == GOSSIP_ACTION_INFO_DEF + 2) {
+        player->InterruptNonMeleeSpells(false);
+        player->CastSpell(player, SPELL_TELEPORT_UDALO, false);
+    }
 
-    player->InterruptNonMeleeSpells(false);
-    player->CastSpell(player, SPELL_TELEPORT, false);
     return true;
 }
 
 /*######
 ## npc_spirit_of_udalo
 ######*/
-
-#define SPELL_TELEPORT_UDALO    41570
-#define GOSSIP_UDALO            "Teleportez-moi au Conseil Illidari"
 
 bool GossipHello_npc_spirit_of_udalo(Player* player, Creature* _Creature)
 {
