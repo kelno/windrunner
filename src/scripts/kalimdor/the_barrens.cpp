57f2bbb06a09d80f40aeaab6298a497a1e96e1f9
thumsoul <thumsoul@localhost>
2011-11-29 21:32:39 +0100

    Script for quest 4021

--- src/scripts/kalimdor/the_barrens.cpp
+++ src/scripts/kalimdor/the_barrens.cpp
@@ -17,7 +17,7 @@
 /* ScriptData
 SDName: The_Barrens
 SD%Complete: 90
-SDComment: Quest support: 863, 898, 1719, 2458, 4921, 6981
+SDComment: Quest support: 863, 898, 1719, 2458, 4921, 6981, 4021
 SDCategory: Barrens
 EndScriptData */
 
@@ -27,6 +27,7 @@ npc_sputtervalve
 npc_taskmaster_fizzule
 npc_twiggy_flathead
 npc_wizzlecrank_shredder
+npc_regthar_deathgate
 EndContentData */
 
 #include "precompiled.h"
@@ -655,6 +656,283 @@ CreatureAI* GetAI_npc_wizzlecrank_shredderAI(Creature* pCreature)
     return new npc_wizzlecrank_shredderAI(pCreature);
 }
 
+/*######
+## npc_regthar_deathgate
+######*/
+
+enum RegtharDeathgateData {
+    QUEST_COUNTERATTACK     = 4021,
+    
+    NPC_HORDE_DEFENDER      = 9457,
+    NPC_HORDE_AXE_THROWER   = 9458,
+    NPC_KOLKAR_INVADER      = 9524,
+    NPC_KOLKAR_STORMSEER    = 9523,
+    NPC_LANTI_GAH           = 9990,
+    NPC_WARLOCK_KROMZAR     = 9456,
+    
+    YELL_COUNTERATTACK_LANTIGAH     = -1000800,
+    SAY_COUNTERATTACK_DEFENDER      = -1000801,
+    SAY_COUNTERATTACK_BEWARE        = -1000802,
+    YELL_COUNTERATTACK_KROMZAR      = -1000803
+};
+
+float attackersPos[][4] = { { -301.483246, -1864.453125, 92.982010, 5.107399 },
+                            { -296.849884, -1864.198120, 92.681602, 4.956865 },
+                            { -292.218994, -1863.122681, 92.529182, 4.685903 },
+                            { -288.826233, -1863.246704, 92.524986, 4.675867 },
+                            { -284.970764, -1858.442993, 92.458336, 4.645762 },
+                            { -280.549591, -1858.738037, 92.583649, 4.645762 },
+                            { -286.257660, -1865.968018, 92.625084, 4.645762 },
+                            { -272.915497, -1846.779907, 93.371109, 4.164052 },
+                            { -282.390839, -1843.288086, 92.586586, 4.489207 },
+                            { -290.774078, -1842.121582, 92.882210, 4.736608 },
+                            { -300.243317, -1842.350952, 93.933723, 4.736608 },
+                            { -307.937622, -1843.978638, 94.907982, 5.060192 },
+                            { -316.356171, -1848.410156, 95.201050, 5.319373 },
+                            { -229.504166, -1917.720459, 92.358574, 2.399001 },
+                            { -234.851791, -1921.778442, 92.701218, 2.219930 },
+                            { -238.310852, -1927.177368, 92.959702, 2.571789 },
+                            { -241.936066, -1932.835693, 92.934319, 2.571789 },
+                            { -251.544861, -1933.647949, 91.972511, 2.571789 },
+                            { -252.290527, -1927.605347, 91.928032, 2.571789 },
+                            { -246.580887, -1923.654297, 92.278069, 2.571789 },
+                            { -243.556091, -1918.933105, 92.251320, 2.571789 },
+                            { -243.148834, -1912.324341, 91.964806, 2.571789 },
+                            { -251.019424, -1911.610718, 91.883850, 2.571789 },
+                            { -255.817886, -1916.005737, 91.875778, 2.571789 },
+                            { -260.920074, -1921.308594, 91.817719, 2.571789 },
+                            { -265.865906, -1925.523193, 91.897148, 2.571789 }};
+
+    
+float defendersPos[][4] = {   { -292.977020, -1892.080566, 91.995491, 1.584019 },
+                                { -287.853455, -1892.012817, 92.257652, 1.584019 },
+                                { -280.889069, -1891.920776, 92.060722, 1.584019 },
+                                { -276.943176, -1876.993286, 92.618614, 1.584019 },
+                                { -287.600189, -1872.038208, 92.740288, 1.584019 },
+                                { -247.661148, -1911.826416, 91.863655, 6.151107 },
+                                { -241.494400, -1915.744019, 92.160278, 6.151107 },
+                                { -236.513535, -1912.012329, 92.013985, 6.151107 },
+                                { -235.899582, -1907.390991, 91.823219, 6.151107 },
+                                { -236.531174, -1917.745117, 92.372627, 6.151107 },
+                                { -273.915649, -1925.971069, 92.491234, 1.333126 },
+                                { -276.039703, -1922.492432, 92.560356, 1.333126 },
+                                { -272.844818, -1917.105469, 92.344337, 1.333126 }};
+
+struct npc_regthar_deathgateAI : public ScriptedAI
+{
+    npc_regthar_deathgateAI(Creature* c) : ScriptedAI(c), summons(me) {}
+    
+    bool eventRunning;
+    bool lantigahYelled;
+    bool kromzar;
+    
+    uint8 maxDefenders;
+    uint8 maxAttackers;
+    uint8 defendersCount;
+    uint8 attackersCount;
+    uint8 killsNeeded;
+    uint8 killsCounter;
+    
+    uint32 despawnAllTimer;
+    uint32 defendersTimer;
+    uint32 attackersTimer;
+    
+    uint64 starterGUID;
+    
+    SummonList summons;
+    
+    void Reset()
+    {
+        maxDefenders = 15;
+        maxAttackers = 20;
+        despawnAllTimer = 0;
+        starterGUID = 0;
+        eventRunning = false;
+        lantigahYelled = false;
+        killsNeeded = 0;
+        killsCounter = 0;
+        kromzar = false;
+    }
+    
+    void Aggro(Unit* who) {}
+    
+    void JustSummoned(Creature* summoned)
+    {
+        summons.Summon(summoned);
+        
+        switch (summoned->GetEntry()) {
+            case NPC_KOLKAR_INVADER:
+            case NPC_KOLKAR_STORMSEER:
+                attackersCount++;
+                break;
+            case NPC_HORDE_DEFENDER:
+            case NPC_HORDE_AXE_THROWER:
+                defendersCount++;
+                break;
+            case NPC_WARLOCK_KROMZAR:
+            {
+                if (Player* player = Unit::GetPlayer(starterGUID))
+                    player->CastSpell(player, 13965, true);
+                break;
+            }
+        }
+    }
+    
+    void SummonedCreatureDespawn(Creature* summon)
+    {
+        summons.Despawn(summon);
+        
+        switch (summon->GetEntry()) {
+            case NPC_KOLKAR_INVADER:
+            case NPC_KOLKAR_STORMSEER:
+                attackersCount--;
+                killsCounter++;
+                break;
+            case NPC_HORDE_DEFENDER:
+            case NPC_HORDE_AXE_THROWER:
+                defendersCount--;
+                killsCounter++;
+                if (rand()%10 > 3)
+                    DoScriptText(SAY_COUNTERATTACK_DEFENDER, me, NULL);
+                break;
+            case NPC_WARLOCK_KROMZAR:
+            {
+                if (Player* player = Unit::GetPlayer(starterGUID))
+                    player->CastSpell(player, 13965, true);
+
+                EndEvent();
+                break;
+            }
+        }
+    }
+    
+    void StartEvent(uint64 playerGUID)
+    {
+        if (eventRunning)
+            return;
+            
+        if (Player* player = Unit::GetPlayer(playerGUID))
+            DoScriptText(SAY_COUNTERATTACK_BEWARE, me, player);
+            
+        despawnAllTimer = 0;
+        starterGUID = playerGUID;
+            
+        eventRunning = true;
+        lantigahYelled = false;
+        killsNeeded = 18 + rand()%6;
+        killsCounter = 0;
+        kromzar = false;
+        defendersTimer = 10000;
+        attackersTimer = 8000;
+        
+        for (uint8 i = 0; i < 8; i++)
+            me->SummonCreature(RAND(NPC_HORDE_DEFENDER, NPC_HORDE_AXE_THROWER), defendersPos[rand()%13][0], defendersPos[rand()%13][1], defendersPos[rand()%13][2], defendersPos[rand()%13][3], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 4000);
+            
+        for (uint8 i = 0; i < 6; i++)
+            me->SummonCreature(RAND(NPC_KOLKAR_INVADER, NPC_KOLKAR_STORMSEER), attackersPos[rand()%25][0], attackersPos[rand()%25][1], attackersPos[rand()%25][2], attackersPos[rand()%25][3], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 4000);
+            
+        for (uint8 i = 13; i < 17; i++)
+            me->SummonCreature(RAND(NPC_KOLKAR_INVADER, NPC_KOLKAR_STORMSEER), attackersPos[rand()%25][0], attackersPos[rand()%25][1], attackersPos[rand()%25][2], attackersPos[rand()%25][3], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 4000);
+    }
+    
+    void EndEvent()
+    {
+        despawnAllTimer = 15000;
+        eventRunning = false;
+        killsNeeded = 0;
+        killsCounter = 0;
+        kromzar = false;
+    }
+    
+    void UpdateAI(uint32 const diff)
+    {
+        if (despawnAllTimer) {
+            if (despawnAllTimer <= diff) {
+                summons.DespawnAll();
+                despawnAllTimer = 0;
+            }
+            else
+                despawnAllTimer -= diff;
+        }
+        
+        if (!eventRunning)
+            return;
+
+            
+        Player* player = Unit::GetPlayer(starterGUID);
+        if (!player || player->isDead()) {
+            EndEvent();
+            return;
+        }
+            
+        if (defendersTimer <= diff) {
+            if (defendersCount < maxDefenders)
+                me->SummonCreature(RAND(NPC_HORDE_DEFENDER, NPC_HORDE_AXE_THROWER), defendersPos[rand()%13][0], defendersPos[rand()%13][1], defendersPos[rand()%13][2], defendersPos[rand()%13][3], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 4000);
+            
+            defendersTimer = 10000 + rand()%2000;
+        }
+        else
+            defendersTimer -= diff;
+            
+        if (attackersTimer <= diff) {
+            if (attackersCount < maxAttackers)
+                me->SummonCreature(RAND(NPC_KOLKAR_INVADER, NPC_KOLKAR_STORMSEER), attackersPos[rand()%25][0], attackersPos[rand()%25][1], attackersPos[rand()%25][2], attackersPos[rand()%25][3], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 4000);
+
+            attackersTimer = 8000 + rand()%2000;
+        }
+        else
+            attackersTimer -= diff;
+            
+        if (killsCounter >= killsNeeded/2 && !lantigahYelled) {
+            if (Creature* lantigah = me->FindCreatureInGrid(NPC_LANTI_GAH, 100.0f, true)) {
+                lantigahYelled = true;
+                DoScriptText(YELL_COUNTERATTACK_LANTIGAH, lantigah, NULL);
+            }
+        }
+        
+        if (killsCounter >= killsNeeded && !kromzar) {
+            if (Creature* warlordKromzar = me->SummonCreature(NPC_WARLOCK_KROMZAR, -307.937622, -1843.978638, 94.907982, 5.060192, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 8000)) {
+                kromzar = true;
+                DoScriptText(YELL_COUNTERATTACK_KROMZAR, warlordKromzar, NULL);
+            }
+        }
+    }
+};
+
+CreatureAI* GetAI_npc_regthar_deathgate(Creature* creature)
+{
+    return new npc_regthar_deathgateAI(creature);
+}
+
+bool GossipHello_npc_regthar_deathgate(Player* player, Creature* creature)
+{
+    if (creature->isQuestGiver())
+        player->PrepareQuestMenu(creature->GetGUID());
+
+    if (player->GetQuestStatus(QUEST_COUNTERATTACK) == QUEST_STATUS_INCOMPLETE)
+        player->ADD_GOSSIP_ITEM(0, "Où est le Seigneur de guerre Krom'zar ?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
+        
+    player->SEND_GOSSIP_MENU(creature->GetNpcTextId(), creature->GetGUID());
+    
+    return true;
+}
+
+bool GossipSelect_npc_regthar_deathgate(Player* player, Creature* creature, uint32 sender, uint32 action)
+{
+    if (action == GOSSIP_ACTION_INFO_DEF)
+        ((npc_regthar_deathgateAI*)creature->AI())->StartEvent(player->GetGUID());
+        
+    return true;
+}
+
+bool QuestAccept_npc_regthar_deathgate(Player* player, Creature* creature, const Quest* quest)
+{
+    if (quest->GetQuestId() == QUEST_COUNTERATTACK)
+        ((npc_regthar_deathgateAI*)creature->AI())->StartEvent(player->GetGUID());
+        
+    return true;
+}
+
 void AddSC_the_barrens()
 {
     Script* newscript;
@@ -692,6 +970,14 @@ void AddSC_the_barrens()
     newscript->Name="npc_wizzlecrank_shredder";
     newscript->GetAI = &GetAI_npc_wizzlecrank_shredderAI;
     newscript->pQuestAccept = &QuestAccept_npc_wizzlecrank_shredder;
+    newscript->RegisterSelf();
+    
+    newscript = new Script;
+    newscript->Name = "npc_regthar_deathgate";
+    newscript->GetAI = &GetAI_npc_regthar_deathgate;
+    newscript->pGossipHello = &GossipHello_npc_regthar_deathgate;
+    newscript->pGossipSelect = &GossipSelect_npc_regthar_deathgate;
+    newscript->pQuestAccept = &QuestAccept_npc_regthar_deathgate;
     newscript->RegisterSelf();
 }
 
