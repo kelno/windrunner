536cd75aee93654bbe7fdaf2f52c08fee8cf5c3a
thumsoul <thumsoul@localhost>
2012-04-09 11:36:13 +0200

    Fixed Hunter's Aspects stacking

---
+++ src/game/CreatureAINew.h
@@ -0,0 +1,168 @@
+/*
+ * Copyright (C) 2009 - 2011 Windrunner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef WR_CREATUREAI_H
+#define WR_CREATUREAI_H
+
+enum SelectedTarget
+{
+    TARGET_RANDOM = 0,                               //Just selects a random target
+    TARGET_TOPAGGRO,                                 //Selects targes from top aggro to bottom
+    TARGET_BOTTOMAGGRO,                              //Selects targets from bottom aggro to top
+    TARGET_NEAREST,
+    TARGET_FARTHEST,
+};
+
+#define EVENT_MAX_ID    255
+
+typedef struct aiEvent
+{
+    uint8  id;
+    uint32 timer;
+    uint32 flags;
+    bool   active;
+    bool   activeByDefault;
+    uint32 phaseMask;
+    
+    aiEvent(uint32 _id, uint32 _minTimer, uint32 _maxTimer, uint32 _flags, bool _activeByDefault, uint32 _phaseMask) :
+        id(_id), flags(_flags), activeByDefault(_activeByDefault), active(activeByDefault), phaseMask(_phaseMask)
+    {
+        if (_minTimer > _maxTimer) {
+            sLog.outError("AIEvent::AIEVent: event %u has minTimer > maxTimer, swapping timers.", id);
+            std::swap(_minTimer, _maxTimer);
+        }
+        
+        if (_minTimer != _maxTimer)
+            timer = _minTimer + rand()%(_maxTimer - _minTimer);
+        else
+            timer = _minTimer;
+    }
+    
+    void calcTimer(uint32 minTimer)
+    {
+        calcTimer(minTimer, minTimer);
+    }
+    
+    void calcTimer(uint32 minTimer, uint32 maxTimer)
+    {
+        if (minTimer > maxTimer)
+            std::swap(minTimer, maxTimer);
+            
+        if (minTimer != maxTimer)
+            timer = minTimer + rand()%(maxTimer - minTimer);
+        else
+            timer = minTimer;
+    }
+    
+    bool isActiveInPhase(uint32 phase)
+    {
+        if (phaseMask == 0)
+            return true;
+
+        return ((phaseMask & (1 << phase)) != 0);
+    }
+} AIEvent;
+
+typedef enum eventFlag
+{
+    EVENT_FLAG_DELAY_IF_CASTING  = 0x1
+} EventFlag;
+
+class CreatureAINew
+{
+    public:
+        CreatureAINew(Creature* creature) : me(creature), inCombat(false), m_currEvent(EVENT_MAX_ID), m_phase(0) {}
+
+        virtual ~CreatureAINew();
+        
+        /* Events handling */
+        void addEvent(uint8 id, uint32 minTimer, uint32 maxTimer, uint32 flags = 0, bool activeByDefault = true, uint32 phaseMask = 0);
+        void scheduleEvent(uint8 id, uint32 minTimer, uint32 maxTimer);
+        void scheduleEvent(uint8 id, uint32 timer) { scheduleEvent(id, timer, timer); }
+        void disableEvent(uint8 id);
+        void enableEvent(uint8 id);
+        void delayEvent(uint8 id, uint32 delay);
+        void delayAllEvents(uint32 delay);
+        bool executeEvent(uint32 const /*diff*/, uint8& /*id*/);
+        void updateEvents(uint32 const /*diff*/);
+        // + ensureTimerOnEvents(uint32 minTimer); -> delay events which have timer < minTimer to minTimer
+        
+        /* Phases handling */
+        void setPhase(uint8 phase) { m_phase = phase; onEnterPhase(m_phase); }
+        void incrPhase() { m_phase++; onEnterPhase(m_phase); }
+        void decrPhase() { m_phase--; onEnterPhase(m_phase); }
+        uint8 getPhase() { return m_phase; }
+        uint32 phaseMaskForPhase(uint8 phase) { if (phase > 0) return (1 << phase); }
+
+        bool aiInCombat() { return inCombat; }
+        void setAICombat(bool on) { inCombat = on; }
+        
+        /* Target selection */
+        Unit* selectUnit(SelectedTarget /*target*/, uint32 /*position*/);
+        void getAllPlayersInRange(std::list<Player*>& /*players*/, float /*range*/);
+        
+        void doCast(Unit* /*victim*/, uint32 /*spellId*/, bool triggered = false, bool interrupt = false);
+        void setZoneInCombat();
+        uint32 talk(uint8 /*groupid*/, uint64 targetGUID = 0);
+        void deleteFromThreatList(uint64 /*guid*/);
+        void deleteFromThreatList(Unit* /*target*/);
+
+        /* At every creature update */
+        virtual void update(uint32 const /*diff*/);
+        bool updateVictim(bool evade = true);
+        void doMeleeAttackIfReady();
+        /* In Creature::AIM_Initialize() */
+        virtual void initialize() { onReset(true); }
+        /* When reset (spawn & evade) */
+        virtual void onReset(bool /*onSpawn*/) {}
+        /* When creature respawn */
+        virtual void onRespawn() { onReset(true); }
+        /* When entering evade mode */
+        virtual void evade();
+        /* When reaching home position */
+        virtual void onReachedHome() {}
+        /* When attacking a new target */
+        void attackStart(Unit* /*victim*/);
+        /* When entering combat */
+        virtual void onCombatStart(Unit* /*victim*/) {}
+        /* On death */
+        virtual void onDeath(Unit* /*killer*/) {}
+        /* When killed a unit */
+        virtual void onKill(Unit* /*victim*/) {}
+        /* When another unit is moving in line of sight */
+        virtual void onMoveInLoS(Unit* /*who*/);
+        /* When adding some threat on another unit */
+        virtual void onThreatAdd(Unit* /*who*/, float& /*threat*/) {}
+        /* When removing some threat from another unit */
+        virtual void onThreatRemove(Unit* /*who*/, float& /*threat*/) {}
+        /* When changing phase */
+        virtual void onEnterPhase(uint32 /*newPhase*/) {}
+
+    protected:
+        Creature* me;
+        
+        bool inCombat;
+        
+        typedef std::map<uint8, AIEvent*> EventMap;
+        EventMap m_events;
+        
+        uint8 m_currEvent;
+        uint8 m_phase;
+};
+ 
+#endif
---
+++ src/scripts/kalimdor/darkshore.cpp
@@ -0,0 +1,385 @@
+/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/* ScriptData
+SDName: Darkshore
+SD%Complete: 100
+SDComment: Quest support: 731, 2078, 5321
+SDCategory: Darkshore
+EndScriptData */
+
+/* ContentData
+npc_kerlonian
+npc_threshwackonator
+npc_prospector_remtravel
+EndContentData */
+
+#include "precompiled.h"
+#include "EscortAI.h"
+#include "FollowerAI.h"
+
+/*######
+## npc_kerlonian
+######*/
+
+enum eKerlonian
+{
+    SAY_KER_START               = -1000434,
+
+    EMOTE_KER_SLEEP_1           = -1000435,
+    EMOTE_KER_SLEEP_2           = -1000436,
+    EMOTE_KER_SLEEP_3           = -1000437,
+
+    SAY_KER_SLEEP_1             = -1000438,
+    SAY_KER_SLEEP_2             = -1000439,
+    SAY_KER_SLEEP_3             = -1000440,
+    SAY_KER_SLEEP_4             = -1000441,
+
+    EMOTE_KER_AWAKEN            = -1000445,
+
+    SAY_KER_ALERT_1             = -1000442,
+    SAY_KER_ALERT_2             = -1000443,
+
+    SAY_KER_END                 = -1000444,
+
+    SPELL_SLEEP_VISUAL          = 25148,
+    SPELL_AWAKEN                = 17536,
+    QUEST_SLEEPER_AWAKENED      = 5321,
+    NPC_LILADRIS                = 11219,                    //attackers entries unknown
+    FACTION_KER_ESCORTEE        = 113
+};
+
+//TODO: make concept similar as "ringo" -escort. Find a way to run the scripted attacks, _if_ player are choosing road.
+struct npc_kerlonianAI : public FollowerAI
+{
+    npc_kerlonianAI(Creature* pCreature) : FollowerAI(pCreature) { }
+
+    uint32 m_uiFallAsleepTimer;
+
+    void Reset()
+    {
+        m_uiFallAsleepTimer = urand(10000, 45000);
+    }
+
+    void MoveInLineOfSight(Unit* pWho)
+    {
+        FollowerAI::MoveInLineOfSight(pWho);
+
+        if (!m_creature->getVictim() && !HasFollowState(STATE_FOLLOW_COMPLETE) && pWho->GetEntry() == NPC_LILADRIS)
+        {
+            if (m_creature->IsWithinDistInMap(pWho, INTERACTION_DISTANCE*5))
+            {
+                if (Player* pPlayer = GetLeaderForFollower())
+                {
+                    if (pPlayer->GetQuestStatus(QUEST_SLEEPER_AWAKENED) == QUEST_STATUS_INCOMPLETE)
+                        pPlayer->GroupEventHappens(QUEST_SLEEPER_AWAKENED, m_creature);
+
+                    DoScriptText(SAY_KER_END, m_creature);
+                }
+
+                SetFollowComplete();
+            }
+        }
+    }
+
+    void SpellHit(Unit* pCaster, const SpellEntry* pSpell)
+    {
+        if (HasFollowState(STATE_FOLLOW_INPROGRESS | STATE_FOLLOW_PAUSED) && pSpell->Id == SPELL_AWAKEN)
+            ClearSleeping();
+    }
+
+    void SetSleeping()
+    {
+        SetFollowPaused(true);
+
+        DoScriptText(RAND(EMOTE_KER_SLEEP_1,EMOTE_KER_SLEEP_2,EMOTE_KER_SLEEP_3), m_creature);
+
+        DoScriptText(RAND(SAY_KER_SLEEP_1,SAY_KER_SLEEP_2,SAY_KER_SLEEP_3,SAY_KER_SLEEP_4), m_creature);
+
+        m_creature->SetStandState(UNIT_STAND_STATE_SLEEP);
+        m_creature->CastSpell(m_creature, SPELL_SLEEP_VISUAL, false);
+    }
+
+    void ClearSleeping()
+    {
+        m_creature->RemoveAurasDueToSpell(SPELL_SLEEP_VISUAL);
+        m_creature->SetStandState(UNIT_STAND_STATE_STAND);
+
+        DoScriptText(EMOTE_KER_AWAKEN, m_creature);
+
+        SetFollowPaused(false);
+    }
+
+    void Aggro(Unit* pWho) {}
+
+    void UpdateFollowerAI(const uint32 uiDiff)
+    {
+        if (!UpdateVictim())
+        {
+            if (!HasFollowState(STATE_FOLLOW_INPROGRESS))
+                return;
+
+            if (!HasFollowState(STATE_FOLLOW_PAUSED))
+            {
+                if (m_uiFallAsleepTimer <= uiDiff)
+                {
+                    SetSleeping();
+                    m_uiFallAsleepTimer = urand(25000, 90000);
+                }
+                else
+                    m_uiFallAsleepTimer -= uiDiff;
+            }
+
+            return;
+        }
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_npc_kerlonian(Creature* pCreature)
+{
+    return new npc_kerlonianAI(pCreature);
+}
+
+bool QuestAccept_npc_kerlonian(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
+{
+    if (pQuest->GetQuestId() == QUEST_SLEEPER_AWAKENED)
+    {
+        if (npc_kerlonianAI* pKerlonianAI = CAST_AI(npc_kerlonianAI, pCreature->AI()))
+        {
+            pCreature->SetStandState(UNIT_STAND_STATE_STAND);
+            DoScriptText(SAY_KER_START, pCreature, pPlayer);
+            pKerlonianAI->StartFollow(pPlayer, FACTION_KER_ESCORTEE, pQuest);
+        }
+    }
+
+    return true;
+}
+
+/*####
+# npc_threshwackonator
+####*/
+
+enum eThreshwackonator
+{
+    EMOTE_START             = -1000413, //signed for 4966
+    SAY_AT_CLOSE            = -1000414, //signed for 4966
+    QUEST_GYROMAST_REV      = 2078,
+    NPC_GELKAK              = 6667,
+    FACTION_HOSTILE         = 14
+};
+
+#define GOSSIP_ITEM_INSERT_KEY  "[PH] Insert key"
+
+struct npc_threshwackonatorAI : public FollowerAI
+{
+    npc_threshwackonatorAI(Creature* pCreature) : FollowerAI(pCreature) { }
+
+    void Reset() { }
+    
+    void Aggro(Unit* pWho) {}
+
+    void MoveInLineOfSight(Unit* pWho)
+    {
+        FollowerAI::MoveInLineOfSight(pWho);
+
+        if (!m_creature->getVictim() && !HasFollowState(STATE_FOLLOW_COMPLETE) && pWho->GetEntry() == NPC_GELKAK)
+        {
+            if (m_creature->IsWithinDistInMap(pWho, 10.0f))
+            {
+                DoScriptText(SAY_AT_CLOSE, pWho);
+                DoAtEnd();
+            }
+        }
+    }
+
+    void DoAtEnd()
+    {
+        me->setFaction(FACTION_HOSTILE);
+
+        SetFollowComplete(true);
+
+        if (Player* pHolder = GetLeaderForFollower())
+            me->AI()->AttackStart(pHolder);
+    }
+};
+
+CreatureAI* GetAI_npc_threshwackonator(Creature* pCreature)
+{
+    return new npc_threshwackonatorAI(pCreature);
+}
+
+bool GossipHello_npc_threshwackonator(Player* pPlayer, Creature* pCreature)
+{
+    if (pPlayer->GetQuestStatus(QUEST_GYROMAST_REV) == QUEST_STATUS_INCOMPLETE)
+        pPlayer->ADD_GOSSIP_ITEM(0, GOSSIP_ITEM_INSERT_KEY, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+
+    pPlayer->SEND_GOSSIP_MENU(pCreature->GetNpcTextId(), pCreature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_npc_threshwackonator(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    if (uiAction == GOSSIP_ACTION_INFO_DEF+1)
+    {
+        pPlayer->CLOSE_GOSSIP_MENU();
+
+        if (npc_threshwackonatorAI* pThreshAI = CAST_AI(npc_threshwackonatorAI, pCreature->AI()))
+        {
+            DoScriptText(EMOTE_START, pCreature);
+            pThreshAI->StartFollow(pPlayer);
+        }
+    }
+
+    return true;
+}
+
+/*####
+# npc_prospector_remtravel
+####*/
+
+enum eRemtravel
+{
+    SAY_REM_START               = -1000327,
+    SAY_REM_RAMP1_1             = -1000328,
+    SAY_REM_RAMP1_2             = -1000329,
+    SAY_REM_BOOK                = -1000330,
+    SAY_REM_TENT1_1             = -1000331,
+    SAY_REM_TENT1_2             = -1000332,
+    SAY_REM_MOSS                = -1000333,
+    EMOTE_REM_MOSS              = -1000334,
+    SAY_REM_MOSS_PROGRESS       = -1000335,
+    SAY_REM_PROGRESS            = -1000336,
+    SAY_REM_REMEMBER            = -1000337,
+    EMOTE_REM_END               = -1000338,
+    SAY_REM_AGGRO               = -1000339,
+
+    FACTION_ESCORTEE            = 10,
+    QUEST_ABSENT_MINDED_PT2     = 731,
+    NPC_GRAVEL_SCOUT            = 2158,
+    NPC_GRAVEL_BONE             = 2159,
+    NPC_GRAVEL_GEO              = 2160
+};
+
+struct npc_prospector_remtravelAI : public npc_escortAI
+{
+    npc_prospector_remtravelAI(Creature* pCreature) : npc_escortAI(pCreature) {}
+
+    void WaypointReached(uint32 i)
+    {
+        Player* pPlayer = GetPlayerForEscort();
+
+        if (!pPlayer)
+            return;
+
+        switch(i)
+        {
+            case 0:
+                DoScriptText(SAY_REM_START, me, pPlayer);
+                break;
+            case 1:
+                DoScriptText(SAY_REM_RAMP1_1, me, pPlayer);
+                break;
+            case 2:
+                DoSpawnCreature(NPC_GRAVEL_SCOUT, -10.0f, 5.0f, 0.0f, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+                DoSpawnCreature(NPC_GRAVEL_BONE, -10.0f, 7.0f, 0.0f, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+                break;
+            case 4:
+                DoScriptText(SAY_REM_RAMP1_2, me, pPlayer);
+                break;
+            case 7:
+                DoScriptText(SAY_REM_TENT1_1, me, pPlayer);
+                DoSpawnCreature(NPC_GRAVEL_SCOUT, -10.0f, 5.0f, 0.0f, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+                DoSpawnCreature(NPC_GRAVEL_BONE, -10.0f, 7.0f, 0.0f, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+                break;
+            case 8:
+                DoScriptText(SAY_REM_TENT1_2, me, pPlayer);
+                break;
+            case 14:
+                DoScriptText(SAY_REM_MOSS_PROGRESS, me, pPlayer);
+                break;
+            case 15:
+                DoSpawnCreature(NPC_GRAVEL_SCOUT, -15.0f, 3.0f, 0.0f, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+                DoSpawnCreature(NPC_GRAVEL_BONE, -15.0f, 5.0f, 0.0f, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+                DoSpawnCreature(NPC_GRAVEL_GEO, -15.0f, 7.0f, 0.0f, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+                break;
+            case 18:
+                DoScriptText(SAY_REM_BOOK, me, pPlayer);
+                break;
+            case 24:
+                DoScriptText(SAY_REM_REMEMBER, me, pPlayer);
+                break;
+            case 25:
+                DoScriptText(EMOTE_REM_END, me, pPlayer);
+                pPlayer->GroupEventHappens(QUEST_ABSENT_MINDED_PT2, me);
+                break;
+        }
+    }
+
+    void Reset() {}
+
+    void Aggro(Unit* who)
+    {
+        if (rand()%2)
+            DoScriptText(SAY_REM_AGGRO, me, who);
+    }
+};
+
+CreatureAI* GetAI_npc_prospector_remtravel(Creature* pCreature)
+{
+    return new npc_prospector_remtravelAI(pCreature);
+}
+
+bool QuestAccept_npc_prospector_remtravel(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
+{
+    if (pQuest->GetQuestId() == QUEST_ABSENT_MINDED_PT2)
+    {
+        ((npc_escortAI*)(pCreature->AI()))->Start(true, true, false, pPlayer->GetGUID(), pCreature->GetEntry());
+        pCreature->setFaction(FACTION_ESCORTEE);
+    }
+
+    return true;
+}
+
+/*######
+## AddSC
+######*/
+
+void AddSC_darkshore()
+{
+    Script* newscript;
+
+    newscript = new Script;
+    newscript->Name = "npc_kerlonian";
+    newscript->GetAI = &GetAI_npc_kerlonian;
+    newscript->pQuestAccept = &QuestAccept_npc_kerlonian;
+    newscript->RegisterSelf();
+    
+    newscript = new Script;
+    newscript->Name = "npc_threshwackonator";
+    newscript->GetAI = &GetAI_npc_threshwackonator;
+    newscript->pGossipHello = &GossipHello_npc_threshwackonator;
+    newscript->pGossipSelect = &GossipSelect_npc_threshwackonator;
+    newscript->pGossipSelect = &GossipSelect_npc_threshwackonator;
+    newscript->RegisterSelf();
+    
+    newscript = new Script;
+    newscript->Name = "npc_prospector_remtravel";
+    newscript->GetAI = &GetAI_npc_prospector_remtravel;
+    newscript->pQuestAccept = &QuestAccept_npc_prospector_remtravel;
+    newscript->RegisterSelf();
+}
