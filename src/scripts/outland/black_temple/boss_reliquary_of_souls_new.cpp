365050a945e06f1fee212f3eca31f5104bee3772
thumsoul <thumsoul@localhost>
2012-06-09 23:14:28 +0200

    Black Temple: Ros rewrite - second and last step

--- src/scripts/outland/black_temple/boss_reliquary_of_souls_new.cpp
+++ src/scripts/outland/black_temple/boss_reliquary_of_souls_new.cpp
@@ -2,33 +2,28 @@
 #include "def_black_temple.h"
 #include "CreatureScript.h"
 #include "CreatureAINew.h"
+#include "Spell.h"
 
 enum ReliquaryOfSoulsData {
     // Essence of Suffering
     TALK_SUFF_SAY_FREED             = 0,
     TALK_SUFF_SAY_AGGRO             = 1,
-    TALK_SUFF_SAY_SLAY1             = 2,
-    TALK_SUFF_SAY_SLAY2             = 3,
-    TALK_SUFF_SAY_SLAY3             = 4,
-    TALK_SUFF_SAY_RECAP             = 5,
-    TALK_SUFF_SAY_AFTER             = 6,
-    TALK_SUFF_EMOTE_ENRAGE          = 7,
+    TALK_SUFF_SAY_SLAY              = 2,
+    TALK_SUFF_SAY_RECAP             = 3,
+    TALK_SUFF_SAY_AFTER             = 4,
+    TALK_SUFF_EMOTE_ENRAGE          = 5,
     // Essence of Desire
     TALK_DESI_SAY_FREED             = 0,
-    TALK_DESI_SAY_SLAY1             = 1,
-    TALK_DESI_SAY_SLAY2             = 2,
-    TALK_DESI_SAY_SLAY3             = 3,
-    TALK_DESI_SAY_SPEC              = 4,
-    TALK_DESI_SAY_RECAP             = 5,
-    TALK_DESI_SAY_AFTER             = 6,
+    TALK_DESI_SAY_SLAY              = 1,
+    TALK_DESI_SAY_SPEC              = 2,
+    TALK_DESI_SAY_RECAP             = 3,
+    TALK_DESI_SAY_AFTER             = 4,
     // Essence of Anger
     TALK_ANGER_SAY_FREED            = 0,
-    TALK_ANGER_SAY_FREED2           = 1,
-    TALK_ANGER_SAY_SLAY1            = 2,
-    TALK_ANGER_SAY_SLAY2            = 3,
-    TALK_ANGER_SAY_SPEC             = 4,
-    TALK_ANGER_SAY_BEFORE           = 5,
-    TALK_ANGER_SAY_DEATH            = 6,
+    TALK_ANGER_SAY_SLAY             = 1,
+    TALK_ANGER_SAY_SPEC             = 2,
+    TALK_ANGER_SAY_BEFORE           = 3,
+    TALK_ANGER_SAY_DEATH            = 4,
     
     // Spells
     AURA_OF_SUFFERING               = 41292,
@@ -60,6 +55,7 @@ enum ReliquaryOfSoulsData {
     // Misc
     CREATURE_ENSLAVED_SOUL          = 23469,
     NUMBER_ENSLAVED_SOUL            = 8,
+    DATA_SOUL_DEATH                 = 0,
     
     // Phases
     PHASE_NONE                      = 0,
@@ -78,7 +74,7 @@ static float soulsPos[][2] = { {450.4, 212.3},
 class Boss_reliquary_of_souls : public CreatureScript
 {
 public:
-    Boss_reliquary_of_souls() : CreatureScript("boss_reliquary_of_souls_new") {}
+    Boss_reliquary_of_souls() : CreatureScript("boss_reliquary_of_souls") {}
     
     class Boss_reliquary_of_soulsAI : public CreatureAINew
     {
@@ -174,6 +170,12 @@ public:
             }
         }
         
+        void message(uint32 id, uint32 data)
+        {
+            if (id == DATA_SOUL_DEATH && data == 1)
+                soulDeathCount++;
+        }
+        
         void update(uint32 const diff)
         {
             if (phase == PHASE_NONE)
@@ -228,7 +230,6 @@ public:
                     else {
                         if (essence->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE)) {
                             mergeThreatList(essence);
-                            essence->RemoveAllAuras();
                             essence->DeleteThreatList();
                             essence->SetReactState(REACT_PASSIVE);
                             essence->GetMotionMaster()->MoveFollow(me, 0, 0);
@@ -239,7 +240,7 @@ public:
                     timer = 1000;
                     break;
                 case 4:
-                    timer = 1500;
+                    timer = 500;
                     if (essence->IsWithinDistInMap(me, 10.0f)) {
                         timer = 2000;
                         me->RemoveAurasDueToSpell(SPELL_SUBMERGE);
@@ -247,10 +248,10 @@ public:
                     }
                     else { // Really necessary?
                         mergeThreatList(essence);
-                        essence->RemoveAllAuras();
                         essence->DeleteThreatList();
                         essence->SetReactState(REACT_PASSIVE);
                         essence->GetMotionMaster()->MoveFollow(me, 0, 0);
+                        return;
                     }
                     break;
                 case 5:
@@ -316,3 +317,391 @@ public:
         return new Boss_reliquary_of_soulsAI(creature);
     }
 };
+
+class Boss_essence_of_suffering : public CreatureScript
+{
+public:
+    Boss_essence_of_suffering() : CreatureScript("boss_essence_of_suffering") {}
+    
+    class Boss_essence_of_sufferingAI : public CreatureAINew
+    {
+    public:
+        Boss_essence_of_sufferingAI(Creature* creature) : CreatureAINew(creature) {}
+        
+        enum events {
+            EV_FIXATE       = 0,
+            EV_ENRAGE       = 1,
+            EV_SOUL_DRAIN   = 2
+        };
+        
+        void onReset(bool onSpawn)
+        {
+            if (onSpawn) {
+                addEvent(EV_FIXATE, 8000, 8000, EVENT_FLAG_DELAY_IF_CASTING);
+                addEvent(EV_ENRAGE, 30000, 30000, EVENT_FLAG_DELAY_IF_CASTING);
+                addEvent(EV_SOUL_DRAIN, 45000, 45000, EVENT_FLAG_DELAY_IF_CASTING);
+            }
+            else {
+                scheduleEvent(EV_FIXATE, 8000);
+                scheduleEvent(EV_ENRAGE, 30000);
+                scheduleEvent(EV_SOUL_DRAIN, 45000);
+            }
+            
+            me->SetFullTauntImmunity(true);
+        }
+        
+        void evade()
+        {
+            if (Creature* reliquary = me->GetSummoner()->ToCreature())
+                reliquary->getAI()->evade();
+                
+            CreatureAINew::evade();
+        }
+        
+        void onDamageTaken(Unit* /*attacker*/, uint32& damage)
+        {
+            if (damage >= me->GetHealth()) {
+                damage = 0;
+                me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+                me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+                me->RemoveAllAuras();
+                talk(TALK_SUFF_SAY_RECAP);
+            }
+        }
+        
+        void onCombatStart(Unit* victim)
+        {
+            talk(TALK_SUFF_SAY_FREED);
+            setZoneInCombat();
+            doCast(me, AURA_OF_SUFFERING, true);
+            doCast(me, ESSENCE_OF_SUFFERING_PASSIVE, true);
+            doCast(me, ESSENCE_OF_SUFFERING_PASSIVE2, true);
+        }
+        
+        void onKill(Unit* victim)
+        {
+            talk(TALK_SUFF_SAY_SLAY);
+        }
+        
+        void update(uint32 const diff)
+        {
+            if (me->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE))
+                return;
+
+            if (!updateVictim())
+                return;
+                
+            updateEvents(diff);
+            
+            while (executeEvent(diff, m_currEvent)) {
+                switch (m_currEvent) {
+                case EV_FIXATE:
+                {
+                    Unit* target = selectUnit(TARGET_NEAREST, 0);
+
+                    if (target) {
+                        target->CastSpell(me, SPELL_FIXATE_TAUNT, true);
+                        doResetThreat();
+                        me->AddThreat(target, 1000000.0f);
+                        
+                        if ((rand() % 16) == 0)
+                            talk(TALK_SUFF_SAY_AGGRO);
+                    }
+                    
+                    scheduleEvent(EV_FIXATE, 5000);
+                    break;
+                }
+                case EV_ENRAGE:
+                    doCast(me, SPELL_ENRAGE);
+                    talk(TALK_SUFF_EMOTE_ENRAGE);
+                    scheduleEvent(EV_ENRAGE, 60000);
+                    break;
+                case EV_SOUL_DRAIN:
+                    doCast(selectUnit(TARGET_RANDOM, 0), SPELL_SOUL_DRAIN);
+                    scheduleEvent(EV_SOUL_DRAIN, 60000);
+                    break;
+                }
+            }
+            
+            doMeleeAttackIfReady();
+        }
+    };
+    
+    CreatureAINew* getAI(Creature* creature)
+    {
+        return new Boss_essence_of_sufferingAI(creature);
+    }
+};
+
+class Boss_essence_of_desire : public CreatureScript
+{
+public:
+    Boss_essence_of_desire() : CreatureScript("boss_essence_of_desire") {}
+    
+    class Boss_essence_of_desireAI : public CreatureAINew
+    {
+    public:
+        Boss_essence_of_desireAI(Creature* creature) : CreatureAINew(creature) {}
+        
+        enum events {
+            EV_RUNE_SHIELD  = 0,
+            EV_DEADEN       = 1,
+            EV_SOUL_SHOCK   = 2
+        };
+        
+        void onReset(bool onSpawn)
+        {
+            if (onSpawn) {
+                addEvent(EV_RUNE_SHIELD, 60000, 60000, EVENT_FLAG_DELAY_IF_CASTING);
+                addEvent(EV_DEADEN, 30000, 30000, EVENT_FLAG_DELAY_IF_CASTING);
+                addEvent(EV_SOUL_SHOCK, 5000, 5000, EVENT_FLAG_DELAY_IF_CASTING);
+            }
+            else {
+                scheduleEvent(EV_RUNE_SHIELD, 60000);
+                scheduleEvent(EV_DEADEN, 30000);
+                scheduleEvent(EV_SOUL_SHOCK, 5000);
+            }
+            
+            me->ApplySpellImmune(0, IMMUNITY_STATE, SPELL_AURA_MOD_CONFUSE, true);
+        }
+        
+        void evade()
+        {
+            if (Creature* reliquary = me->GetSummoner()->ToCreature())
+                reliquary->getAI()->evade();
+                
+            CreatureAINew::evade();
+        }
+        
+        void onDamageTaken(Unit* attacker, uint32& damage)
+        {
+            if (attacker == me)
+                return;
+                
+            if (damage >= me->GetHealth()) {
+                damage = 0;
+                me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+                me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+                me->RemoveAllAuras();
+                talk(TALK_DESI_SAY_RECAP);
+            }
+            else {
+                int32 bp0 = damage / 2;
+                me->CastCustomSpell(attacker, AURA_OF_DESIRE_DAMAGE, &bp0, NULL, NULL, true);
+            }
+        }
+        
+        void onHitBySpell(Unit* caster, SpellEntry const* spell)
+        {
+            if (me->m_currentSpells[CURRENT_GENERIC_SPELL]) {
+                for (uint8 i = 0; i < 3; ++i) {
+                    if (spell->Effect[i] == SPELL_EFFECT_INTERRUPT_CAST) {
+                        if (me->m_currentSpells[CURRENT_GENERIC_SPELL]->m_spellInfo->Id == SPELL_SOUL_SHOCK
+                                || me->m_currentSpells[CURRENT_GENERIC_SPELL]->m_spellInfo->Id == SPELL_DEADEN)
+                            me->InterruptSpell(CURRENT_GENERIC_SPELL, false);
+                    }
+                }
+            }
+        }
+        
+        void onCombatStart(Unit* victim)
+        {
+            talk(TALK_DESI_SAY_FREED);
+            setZoneInCombat();
+            doCast(me, AURA_OF_DESIRE, true);
+        }
+        
+        void onKill(Unit* victim)
+        {
+            talk(TALK_DESI_SAY_SLAY);
+        }
+        
+        void update(uint32 const diff)
+        {
+            if (me->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE))
+                return;
+
+            if (!updateVictim())
+                return;
+                
+            updateEvents(diff);
+            
+            while (executeEvent(diff, m_currEvent)) {
+                switch (m_currEvent) {
+                case EV_RUNE_SHIELD:
+                    doCast(me, SPELL_RUNE_SHIELD, true);
+                    delayEvent(EV_SOUL_SHOCK, 2000);
+                    delayEvent(EV_DEADEN, 2000);
+                    scheduleEvent(EV_RUNE_SHIELD, 60000);
+                    break;
+                case EV_SOUL_SHOCK:
+                    doCast(me->getVictim(), SPELL_SOUL_SHOCK);
+                    scheduleEvent(EV_SOUL_SHOCK, 5000);
+                    break;
+                case EV_DEADEN:
+                    doCast(me->getVictim(), SPELL_DEADEN);
+                    scheduleEvent(EV_DEADEN, 25000, 35000);
+                    if ((rand() % 2) == 0)
+                        talk(TALK_DESI_SAY_SPEC);
+                    break;
+                }
+            }
+            
+            doMeleeAttackIfReady();
+        }
+    };
+    
+    CreatureAINew* getAI(Creature* creature)
+    {
+        return new Boss_essence_of_desireAI(creature);
+    }
+};
+
+class Boss_essence_of_anger : public CreatureScript
+{
+public:
+    Boss_essence_of_anger() : CreatureScript("boss_essence_of_anger") {}
+    
+    class Boss_essence_of_angerAI : public CreatureAINew
+    {
+    public:
+        Boss_essence_of_angerAI(Creature* creature) : CreatureAINew(creature) {}
+        
+        enum events {
+            EV_CHECK_TANK   = 0,
+            EV_SOUL_SCREAM  = 1,
+            EV_SPITE        = 2
+        };
+        
+        void onReset(bool onSpawn)
+        {
+            if (onSpawn) {
+                addEvent(EV_CHECK_TANK, 3000, 3000);
+                addEvent(EV_SOUL_SCREAM, 10000, 10000, EVENT_FLAG_DELAY_IF_CASTING);
+                addEvent(EV_SPITE, 30000, 30000, EVENT_FLAG_DELAY_IF_CASTING);
+            }
+            else {
+                scheduleEvent(EV_CHECK_TANK, 3000);
+                scheduleEvent(EV_SOUL_SCREAM, 10000);
+                scheduleEvent(EV_SPITE, 30000);
+            }
+            
+            tankGUID = 0;
+            spiteGUIDs.clear();
+        }
+        
+        void evade()
+        {
+            if (Creature* reliquary = me->GetSummoner()->ToCreature())
+                reliquary->getAI()->evade();
+                
+            CreatureAINew::evade();
+        }
+        
+        void onCombatStart(Unit* victim)
+        {
+            tankGUID = victim->GetGUID();
+            
+            talk(TALK_ANGER_SAY_FREED);
+            setZoneInCombat();
+            doCast(me, AURA_OF_ANGER, true);
+        }
+        
+        void onDeath(Unit* killer)
+        {
+            talk(TALK_ANGER_SAY_DEATH);
+        }
+        
+        void onKill(Unit* victim)
+        {
+            talk(TALK_ANGER_SAY_SLAY);
+        }
+        
+        void update(uint32 const diff)
+        {
+            if (me->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE))
+                return;
+
+            if (!updateVictim())
+                return;
+                
+            updateEvents(diff);
+            
+            while (executeEvent(diff, m_currEvent)) {
+                switch (m_currEvent) {
+                case EV_CHECK_TANK:
+                    if (me->getVictim()->GetGUID() != tankGUID) {
+                        talk(TALK_ANGER_SAY_BEFORE);
+                        doCast(me, SPELL_SELF_SEETHE, true);
+                        tankGUID = me->getVictim()->GetGUID();
+                    }
+                    scheduleEvent(EV_CHECK_TANK, 1000);
+                    break;
+                case EV_SOUL_SCREAM:
+                    doCast(me->getVictim(), SPELL_SOUL_SCREAM);
+                    if ((rand() % 3) == 0)
+                        talk(TALK_ANGER_SAY_SPEC);
+                    scheduleEvent(EV_SOUL_SCREAM, 9000, 11000);
+                    break;
+                case EV_SPITE:
+                    doCast(me, SPELL_SPITE_TARGET);
+                    talk(TALK_ANGER_SAY_SPEC);
+                    scheduleEvent(EV_SPITE, 30000);
+                    break;
+                }
+            }
+            
+            doMeleeAttackIfReady();
+        }
+        
+    private:
+        uint64 tankGUID;
+        
+        std::list<uint64> spiteGUIDs;
+    };
+    
+    CreatureAINew* getAI(Creature* creature)
+    {
+        return new Boss_essence_of_angerAI(creature);
+    }
+};
+
+class Npc_enslaved_soul : public CreatureScript
+{
+public:
+    Npc_enslaved_soul() : CreatureScript("npc_enslaved_soul") {}
+    
+    class Npc_enslaved_soulAI : public CreatureAINew
+    {
+    public:
+        Npc_enslaved_soulAI(Creature* creature) : CreatureAINew(creature) {}
+        
+        void onCombatStart(Unit* victim)
+        {
+            doCast(me, ENSLAVED_SOUL_PASSIVE, true);
+            setZoneInCombat();
+        }
+        
+        void onDeath(Unit* killer)
+        {
+            if (Creature* reliquary = me->GetSummoner()->ToCreature())
+                reliquary->getAI()->message(DATA_SOUL_DEATH, 1);
+                
+            doCast(me, SPELL_SOUL_RELEASE, true);
+        }
+    };
+    
+    CreatureAINew* getAI(Creature* creature)
+    {
+        return new Npc_enslaved_soulAI(creature);
+    }
+};
+
+void addSC_boss_reliquary_of_souls()
+{
+    sScriptMgr.addScript(new Boss_reliquary_of_souls());
+    sScriptMgr.addScript(new Boss_essence_of_suffering());
+    sScriptMgr.addScript(new Boss_essence_of_desire());
+    sScriptMgr.addScript(new Boss_essence_of_anger());
+    sScriptMgr.addScript(new Npc_enslaved_soul());
+}
