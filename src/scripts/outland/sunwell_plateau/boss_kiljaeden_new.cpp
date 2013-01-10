d0512d1cbfcdd1e085d7d9e84c662dc29c560d0c
mathman <mathman@localhost>
2013-01-10 18:05:38 +0100

    Script/KilJaeden : Fix error

--- src/scripts/outland/sunwell_plateau/boss_kiljaeden_new.cpp
+++ src/scripts/outland/sunwell_plateau/boss_kiljaeden_new.cpp
@@ -489,8 +489,6 @@ public:
                 shadowSpike = false;
                 OrbActivated = false;
                 IsWaiting = false;
-                me->ApplySpellImmune(12889, IMMUNITY_STATE, SPELL_AURA_MOD_CASTING_SPEED, true);
-                me->ApplySpellImmune(12889, IMMUNITY_STATE, SPELL_AURA_MOD_LANGUAGE, true);
                 me->SetFullTauntImmunity(true);
                 me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
                 me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
@@ -1486,6 +1484,8 @@ public:
                 else
                     resetEvent(EVENT_STUN, 2000);
 
+                me->ApplySpellImmune(12889, IMMUNITY_STATE, SPELL_AURA_MOD_CASTING_SPEED, true);
+                me->ApplySpellImmune(12889, IMMUNITY_STATE, SPELL_AURA_MOD_LANGUAGE, true);
                 me->addUnitState(UNIT_STAT_STUNNED);
             }
 
