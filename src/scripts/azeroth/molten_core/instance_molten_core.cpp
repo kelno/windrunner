eb9a2226f8da5a2094e9f4ef9d7aa85853ed3afb
thumsoul <thumsoul@localhost>
2013-01-22 18:42:40 +0100

    MoltenCore: Rewritten Lucifron script

--- src/scripts/azeroth/molten_core/instance_molten_core.cpp
+++ src/scripts/azeroth/molten_core/instance_molten_core.cpp
@@ -40,7 +40,7 @@ EndScriptData */
 
 struct instance_molten_core : public ScriptedInstance
 {
-     instance_molten_core(Map *map) : ScriptedInstance(map) {Initialize();};
+     instance_molten_core(Map *map) : ScriptedInstance(map) {}
 
         uint64 Lucifron, Magmadar, Gehennas, Garr, Geddon, Shazzrah, Sulfuron, Golemagg, Domo, Ragnaros, FlamewakerPriest;
         uint64 RuneKoro, RuneZeth, RuneMazj, RuneTheri, RuneBlaz, RuneKress, RuneMohn;
@@ -192,6 +192,9 @@ struct instance_molten_core : public ScriptedInstance
 
         uint32 GetData(uint32 type)
         {
+            if (type >= DATA_LUCIFRON && type <= 9 /*TODO: probably DATA_RAGNAROS*/)
+                return Encounter[type];
+            
             switch(type)
             {
                 case DATA_LUCIFRONISDEAD:
@@ -247,6 +250,9 @@ struct instance_molten_core : public ScriptedInstance
         {
             if (type == DATA_GOLEMAGG_DEATH)
                 IsBossDied[7] = true;
+            
+            if (type >= DATA_LUCIFRON && type <= 9 /*TODO: probably DATA_RAGNAROS*/)
+                Encounter[type] = data;
         }
 };
 
