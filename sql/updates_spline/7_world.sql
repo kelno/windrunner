7ec83248ce2c26604f6cb1190d2927a6af60922d
mathman <mathman@localhost>
2013-09-01 22:54:39 +0200

    Fix mana for felmyst

--- sql/updates_spline/7_world.sql
+++ sql/updates_spline/7_world.sql
@@ -3,3 +3,4 @@ INSERT INTO `spell_linked_spell` VALUES (45856, 49725, 0, 'Breath: Haste');
 
 DELETE FROM `creature_scripts` WHERE `entryorguid` = 25653;
 
+UPDATE `creature_template` SET `minmana` = '3387', `maxmana` = '3387' WHERE `entry` = 25038;
