53d8f9bd0f1fac46c5888dda510f83fbc72a00c2
Kelno <dr.kelno@gmail.com>
2013-10-31 18:50:09 +0100

    Trying to fix Thrall wolves summoning in Hyjal

--- src/scripts/kalimdor/caverns_of_time/hyjal/hyjal.cpp
+++ src/scripts/kalimdor/caverns_of_time/hyjal/hyjal.cpp
@@ -17,7 +17,7 @@
 /* ScriptData
 SDName: Hyjal
 SD%Complete: 80
-SDComment: gossip text id's unknown
+SDComment: 
 SDCategory: Caverns of Time, Mount Hyjal
 EndScriptData */
 
@@ -146,7 +146,7 @@ CreatureAI* GetAI_npc_thrall(Creature *_Creature)
 
     ai->Spell[1].SpellId = SPELL_SUMMON_DIRE_WOLF;
     ai->Spell[1].Cooldown = 60000;
-    ai->Spell[1].TargetType = TARGETTYPE_RANDOM;
+    ai->Spell[1].TargetType = TARGETTYPE_SELF;
 
     return ai;
 }
