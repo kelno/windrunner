6db0c2d5c8a244fe25ff0d92ba3b9b26d6db47d3
Kelno <Kelno@users.noreply.github.com>
2014-01-05 19:29:52 +0100

    Fixed some Spell Los issues ?

---
+++ src/scripts/custom/custom_training_dummy.cpp
@@ -0,0 +1,116 @@
+/* Copyright (C) 2006 - 2008 WoWMania Core <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/* ScriptData
+SDName: npc_training_dummy
+SD%Complete: 100
+SDComment: Custom NPC Training Dummy, like in Wotlk
+SDCategory: Custom
+EndScriptData */
+
+#include "precompiled.h"
+
+typedef UNORDERED_MAP<uint64, uint32> AttackerMap;
+
+struct npc_training_dummy : Scripted_NoMovementAI
+{
+    npc_training_dummy(Creature *c) : Scripted_NoMovementAI(c)
+    {
+        m_entry = c->GetEntry();
+    }
+
+    uint64 m_entry;
+    
+    AttackerMap attackers;
+    
+    void Reset()
+    {
+        m_creature->ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_KNOCK_BACK, true);
+        m_creature->SetHealth(m_creature->GetMaxHealth()/5);
+        me->SetStunned(true);
+    }
+
+    void DamageTaken(Unit* done_by, uint32& damage)
+    {
+        if (done_by->GetTypeId() == TYPEID_PLAYER)
+            attackers[done_by->GetGUID()] = 8000;
+        else if (done_by->ToCreature() && done_by->ToCreature()->isPet()) {
+            if (Unit* owner = done_by->ToCreature()->GetOwner())
+                attackers[owner->GetGUID()] = 8000;
+        }
+
+        if (m_creature->GetHealth() < (m_creature->GetMaxHealth()/10.0f) || m_creature->GetHealth() > (m_creature->GetMaxHealth()/5.0f)) // allow players using finishers
+            m_creature->SetHealth(m_creature->GetMaxHealth()/5);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        for (AttackerMap::iterator itr = attackers.begin(); itr != attackers.end();)
+        {
+            if (itr->second <= diff)
+            {
+                if (Player* attacker = Unit::GetPlayer(itr->first))
+                {
+                    attacker->CombatStop(true);
+                    attacker->AttackStop();
+                    attacker->CombatStopWithPets(true);
+                    attacker->ClearInCombat();
+                }
+
+                itr = attackers.erase(itr);
+
+                if (attackers.empty())
+                {
+                    EnterEvadeMode();
+                    m_creature->SetHealth(m_creature->GetMaxHealth()/5);
+                }
+            }
+            else
+            {
+                itr->second -= diff;
+                ++itr;
+            }
+        }
+    }
+};
+
+bool ReceiveEmote_npc_training_dummy(Player *player, Creature *c, uint32 emote)
+{
+    if (emote == TEXTEMOTE_HUG)
+    {
+        char w[50];
+        snprintf(w, 50, "Menace envers vous : %f",c->getThreatManager().getThreat(player));
+        c->MonsterWhisper(w, player->GetGUID());
+    }
+
+    return true;
+}
+
+CreatureAI* GetAI_npc_training_dummy(Creature* pCreature)
+{
+    return new npc_training_dummy(pCreature);
+}
+
+void AddSC_training_dummy()
+{
+    Script* newscript;
+    
+    newscript = new Script;
+    newscript->Name = "npc_training_dummy";
+    newscript->GetAI = &GetAI_npc_training_dummy;
+    newscript->pReceiveEmote = &ReceiveEmote_npc_training_dummy;
+    newscript->RegisterSelf();
+}
---
+++ src/scripts/kalimdor/caverns_of_time/hyjal/boss_azgalor.cpp
@@ -0,0 +1,322 @@
+#include "precompiled.h"
+#include "def_hyjal.h"
+#include "hyjal_trash.h"
+
+enum Spells
+{
+    SPELL_RAIN_OF_FIRE      = 31340,
+    SPELL_DOOM              = 31347,
+    SPELL_HOWL_OF_AZGALOR   = 31344,
+    SPELL_CLEAVE            = 31345,
+    SPELL_BERSERK           = 26662,
+};
+
+enum Timers
+{
+    TIMER_BERSERK            = 600000,
+    TIMER_DOOM               = 45000,
+    TIMER_CLEAVE             = 5000,
+    TIMER_HOWL_OF_AZGALOR    = 15000,
+    TIMER_RAIN_OF_FIRE       = 13000,
+    TIMER_RAIN_OF_FIRE_FIRST = 30000,
+};
+
+enum Texts
+{
+	SAY_ONDEATH = -1801022,
+	SAY_ONSLAY1 = -1801023,
+	SAY_ONSLAY2 = -1801024,
+	SAY_ONSLAY3 = -1801025,
+	SAY_DOOM1 = -1801026,
+	SAY_DOOM2 = -1801027,
+	SAY_ONAGGRO = -1801028
+};
+
+struct boss_azgalorAI : public hyjal_trashAI
+{
+    boss_azgalorAI(Creature *c) : hyjal_trashAI(c)
+    {
+        pInstance = ((ScriptedInstance*)c->GetInstanceData());
+        go = false;
+        pos = 0;
+        SpellEntry *TempSpell = (SpellEntry*)spellmgr.LookupSpell(SPELL_HOWL_OF_AZGALOR);
+        if(TempSpell)
+            TempSpell->EffectRadiusIndex[0] = 12;//100yards instead of 50000?!
+    }
+
+    uint32 RainTimer;
+    uint32 DoomTimer;
+    uint32 HowlTimer;
+    uint32 CleaveTimer;
+    uint32 EnrageTimer;
+
+    bool go;
+    uint32 pos;
+
+    void Reset()
+    {
+        damageTaken = 0;
+        RainTimer = TIMER_RAIN_OF_FIRE_FIRST;
+        DoomTimer = TIMER_DOOM;
+        HowlTimer = TIMER_HOWL_OF_AZGALOR;
+        CleaveTimer = TIMER_CLEAVE;
+        EnrageTimer = TIMER_BERSERK;
+
+        if(pInstance && IsEvent)
+            pInstance->SetData(DATA_AZGALOREVENT, NOT_STARTED);
+    }
+
+    void Aggro(Unit *who)
+    {
+        if(pInstance && IsEvent)
+            pInstance->SetData(DATA_AZGALOREVENT, IN_PROGRESS);
+
+        DoScriptText(SAY_ONAGGRO,me);
+    }
+
+    void KilledUnit(Unit *victim)
+    {
+        DoScriptText(SAY_ONSLAY1 - rand()%3,me);
+    }
+
+    void WaypointReached(uint32 i)
+    {
+        pos = i;
+        if (i == 7 && pInstance)
+        {
+            Unit* target = Unit::GetUnit((*m_creature), pInstance->GetData64(DATA_THRALL));
+            if (target && target->isAlive())
+                m_creature->AddThreat(target,0.0);
+        }
+    }
+
+    void JustDied(Unit *victim)
+    {
+        hyjal_trashAI::JustDied(victim);
+        if(pInstance && IsEvent)
+            pInstance->SetData(DATA_AZGALOREVENT, DONE);
+
+        DoScriptText(SAY_ONDEATH,me);
+    }
+
+    Unit* SelectDoomTarget()
+    {
+        std::list<Unit*> target;
+        SelectUnitList(target, 2, SELECT_TARGET_RANDOM, 150.0f, true); //get two target, just in case the first one is our tank
+        if(target.size() < 2)
+            return NULL;
+
+        Unit* firstVictim = *(target.begin());
+        Unit* secondVictim = *(target.begin()++);
+        if( firstVictim != me->getVictim() )
+            return firstVictim;
+        else
+            return secondVictim;
+    }
+
+    bool HasTwoPlayersInFront()
+    {
+        uint8 playersInArc = 0;
+        std::list<HostilReference*> const& threatlist = me->getThreatManager().getThreatList();
+
+        for (auto itr : threatlist)
+        {
+            Unit* unit = itr->getTarget();
+            if  (   unit->ToPlayer()
+                && me->GetDistance2d(unit) < 8.0f
+                && me->HasInArc(M_PI,unit)
+                )
+                playersInArc++;
+
+            if(playersInArc > 1)
+                return true;
+        }
+
+        return false;
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if (IsEvent)
+        {
+            //Must update npc_escortAI
+            npc_escortAI::UpdateAI(diff);
+            if(!go)
+            {
+                go = true;
+                if(pInstance)
+                {
+                    ((npc_escortAI*)(m_creature->AI()))->AddWaypoint(0, 5492.91,    -2404.61,    1462.63);
+                    ((npc_escortAI*)(m_creature->AI()))->AddWaypoint(1, 5531.76,    -2460.87,    1469.55);
+                    ((npc_escortAI*)(m_creature->AI()))->AddWaypoint(2, 5554.58,    -2514.66,    1476.12);
+                    ((npc_escortAI*)(m_creature->AI()))->AddWaypoint(3, 5554.16,    -2567.23,    1479.90);
+                    ((npc_escortAI*)(m_creature->AI()))->AddWaypoint(4, 5540.67,    -2625.99,    1480.89);
+                    ((npc_escortAI*)(m_creature->AI()))->AddWaypoint(5, 5508.16,    -2659.2,    1480.15);
+                    ((npc_escortAI*)(m_creature->AI()))->AddWaypoint(6, 5489.62,    -2704.05,    1482.18);
+                    ((npc_escortAI*)(m_creature->AI()))->AddWaypoint(7, 5457.04,    -2726.26,    1485.10);
+                    ((npc_escortAI*)(m_creature->AI()))->Start(false, true, true);
+                    ((npc_escortAI*)(m_creature->AI()))->SetDespawnAtEnd(false);
+                }
+            }
+        }
+
+        //Return since we have no target
+        if (!UpdateVictim() )
+            return;
+
+        if(RainTimer < diff)
+        {
+            if(DoCast(SelectUnit(SELECT_TARGET_RANDOM,0,30,true), SPELL_RAIN_OF_FIRE) == SPELL_CAST_OK)
+                RainTimer = TIMER_RAIN_OF_FIRE;
+        }else RainTimer -= diff;
+
+        if(DoomTimer < diff)
+        {
+            if(DoCast(SelectDoomTarget(), SPELL_DOOM) == SPELL_CAST_OK)
+            {
+                DoomTimer = TIMER_DOOM;
+                DoScriptText(SAY_DOOM1 - rand()%2,me);
+            }
+        }else DoomTimer -= diff;
+
+        if(HowlTimer < diff)
+        {
+            if(DoCast(m_creature, SPELL_HOWL_OF_AZGALOR) == SPELL_CAST_OK)
+                HowlTimer = TIMER_HOWL_OF_AZGALOR;
+        }else HowlTimer -= diff;
+
+        if(CleaveTimer < diff)
+        {
+            if(HasTwoPlayersInFront())
+                if(DoCast(m_creature->getVictim(), SPELL_CLEAVE) == SPELL_CAST_OK)
+                    CleaveTimer = TIMER_CLEAVE;
+        }else CleaveTimer -= diff;
+
+        if(EnrageTimer < diff)
+        {
+            if(DoCast(m_creature, SPELL_BERSERK, true) == SPELL_CAST_OK)
+                EnrageTimer = TIMER_BERSERK;
+        }else EnrageTimer -= diff;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_boss_azgalor(Creature *_Creature)
+{
+    return new boss_azgalorAI (_Creature);
+}
+
+enum DoomguardSpells
+{
+    SPELL_THRASH       = 12787,
+    SPELL_CRIPPLE      = 31406,
+    SPELL_WARSTOMP     = 31408
+};
+
+struct mob_lesser_doomguardAI : public hyjal_trashAI
+{
+    mob_lesser_doomguardAI(Creature *c) : hyjal_trashAI(c)
+    {
+        pInstance = ((ScriptedInstance*)c->GetInstanceData());
+        if(pInstance)
+            AzgalorGUID = pInstance->GetData64(DATA_AZGALOR);
+    }
+
+    uint32 CrippleTimer;
+    uint32 WarstompTimer;
+    uint32 CheckTimer;
+    uint64 AzgalorGUID;
+    ScriptedInstance* pInstance;
+
+    void Reset()
+    {
+        CrippleTimer = 50000;
+        WarstompTimer = 10000;
+        DoCast(m_creature, SPELL_THRASH);
+        CheckTimer = 5000;
+    }
+
+    void Aggro(Unit *who)
+    {
+    }
+
+    void KilledUnit(Unit *victim)
+    {
+
+    }
+
+    void WaypointReached(uint32 i)
+    {
+
+    }
+
+    void MoveInLineOfSight(Unit *who)
+    {
+        if (m_creature->GetDistance(who) <= 50 && !InCombat && m_creature->IsHostileTo(who))
+        {
+            m_creature->AddThreat(who,0.0);
+            m_creature->Attack(who,false);
+        }
+    }
+
+    void JustDied(Unit *victim)
+    {
+
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if(CheckTimer < diff)
+        {
+            if(AzgalorGUID)
+            {
+                Creature* boss = Unit::GetCreature((*m_creature),AzgalorGUID);
+                if(!boss || (boss && boss->isDead()))
+                {
+                    m_creature->setDeathState(JUST_DIED);
+                    m_creature->RemoveCorpse();
+                    return;
+                }
+            }
+            CheckTimer = 5000;
+        }else CheckTimer -= diff;
+
+        //Return since we have no target
+        if (!UpdateVictim() )
+            return;
+
+        if(WarstompTimer < diff)
+        {
+            DoCast(m_creature, SPELL_WARSTOMP);
+            WarstompTimer = 10000+rand()%5000;
+        }else WarstompTimer -= diff;
+
+        if(CrippleTimer < diff)
+        {
+            DoCast(SelectUnit(SELECT_TARGET_RANDOM,0,100,true), SPELL_CRIPPLE);
+            CrippleTimer = 25000+rand()%5000;
+        }else CrippleTimer -= diff;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_mob_lesser_doomguard(Creature *_Creature)
+{
+    return new mob_lesser_doomguardAI (_Creature);
+}
+
+void AddSC_boss_azgalor()
+{
+    Script *newscript;
+    newscript = new Script;
+    newscript->Name="boss_azgalor";
+    newscript->GetAI = &GetAI_boss_azgalor;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="mob_lesser_doomguard";
+    newscript->GetAI = &GetAI_mob_lesser_doomguard;
+    newscript->RegisterSelf();
+}
