f5851ac65653bad7d30fb2d0be6e7ec291120e6e
Kelno <dr.kelno@gmail.com>
2014-01-24 21:39:07 +0100

    Fixed Gruul timers & one crashfix for SMART_ACTION_TALK

--- src/scripts/outland/gruuls_lair/boss_gruul.cpp
+++ src/scripts/outland/gruuls_lair/boss_gruul.cpp
@@ -69,11 +69,11 @@ struct boss_gruulAI : public ScriptedAI
     void Reset()
     {
         Growth_Timer= 30000;
-        CaveIn_Timer= 40000;
-        GroundSlamTimer= 35000;
+        CaveIn_Timer= 10000;
+        GroundSlamTimer= 40000+rand()%5000;
         GroundSlamStage= 0;
         PerformingGroundSlam= false;
-        HurtfulStrike_Timer= 8000;
+        HurtfulStrike_Timer= 20000;
         Reverberation_Timer= 60000+45000;
 
         if(pInstance)
@@ -225,8 +225,8 @@ struct boss_gruulAI : public ScriptedAI
 
                         PerformingGroundSlam = false;
 
-                        GroundSlamTimer =120000;
-                        HurtfulStrike_Timer= 8000;
+                        GroundSlamTimer = 75000;
+                        HurtfulStrike_Timer= 20000;
                         if(Reverberation_Timer < 10000)     //Give a little time to the players to undo the damage from shatter
                             Reverberation_Timer += 10000;
 
@@ -252,7 +252,7 @@ struct boss_gruulAI : public ScriptedAI
                 else
                     DoCast(m_creature->GetVictim(),SPELL_HURTFUL_STRIKE);
 
-                HurtfulStrike_Timer= 8000;
+                HurtfulStrike_Timer= 20000;
             }else HurtfulStrike_Timer -= diff;
 
             // Reverberation
@@ -268,7 +268,7 @@ struct boss_gruulAI : public ScriptedAI
                 if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM,0))
                     DoCast(target,SPELL_CAVE_IN);
 
-                CaveIn_Timer = 20000;
+                CaveIn_Timer = 10000;
             }else CaveIn_Timer -= diff;
 
             // Ground Slam, Gronn Lord's Grasp, Stoned, Shatter
